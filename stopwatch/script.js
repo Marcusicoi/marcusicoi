6549ab33ebd15394d24edf5d8b8aa7510d2b01c6
Glitch (prickle-woodpecker) <none>
2022-02-12 09:47:05 +0000

    💃😹 Checkpoint ./stopwatch/script.js:27704817/397 ./stopwatch/index.html:27704817/102

--- stopwatch/script.js
+++ stopwatch/script.js
@@ -4,7 +4,7 @@
     const stop = document.getElementById('pausebutton');
     const reset = document.getElementById('resetbutton'); 
     //Define Variables
-    var day = 0, hr = 0, min = 0, sec = 0, stoptime = true;
+    var dy = 0, hr = 0, min = 0, sec = 0, stoptime = true;
     //Start the stopwatch
     function startStopwatch() {
       if (stoptime == true) {
@@ -27,14 +27,20 @@
         sec += 1;
         //Time calculations
         if (sec == 60) {
-            min = min + 1;
+            min += 1;
             sec = 0;
         };
         if (min == 60) {
-            hr = hr + 1;
+            hr += 1;
             min = 0;
             sec = 0;
         };
+        if (hr == 24) {
+            dy += 1;
+            hr = 0;
+            min = 0;
+            sec = 0;
+        }
         if (sec < 10 || sec == 0) {
             sec = '0' + sec;
         };
@@ -44,7 +50,11 @@
         if (hr < 10 || hr == 0) {
             hr = '0' + hr;
         };
+        if (dy < 10 || hr == 0) {
+            dy = '0' + dy;
+        };
         //Set the timeout and change the timer by every second
+        timer.getElementsByClassName('days')[0].innerHTML = dy;
         timer.getElementsByClassName('hours')[0].innerHTML = hr;
         timer.getElementsByClassName('minutes')[0].innerHTML = min;
         timer.getElementsByClassName('seconds')[0].innerHTML = sec; 
@@ -53,11 +63,12 @@
    }
 //Reset the stopwatch
 function resetStopwatch() {
+    timer.getElementsByClassName('days')[0].innerHTML = '00';
     timer.getElementsByClassName('hours')[0].innerHTML = '00';
     timer.getElementsByClassName('minutes')[0].innerHTML = '00';
     timer.getElementsByClassName('seconds')[0].innerHTML = '00';
     stoptime = true;
-    sec = 0; min = 0; hr = 0;
+    sec = 0; min = 0; hr = 0; dy = 0;
 };
 //Make the two buttons invisible
 stop.style.visibility = 'hidden';
