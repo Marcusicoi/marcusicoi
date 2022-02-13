4c9322b414a716328266a3f3d69f306cc44dfb9e
Glitch (prickle-woodpecker) <none>
2022-02-13 14:54:18 +0000

    🐏🏪 Checkpoint ./script.js:27704817/742 ./stopwatch/index.html:27704817/1 ./stopwatch/script.js:27704817/1361 ./tileNum/script.js:27704817/1361

--- stopwatch/script.js
+++ stopwatch/script.js
@@ -188,31 +188,9 @@ reset.onclick = function() {
 }; 
 //Use the randomGradient function while the website is onload
 window.onload = function() {randomGradient()};
-//Tile number generator.
-//Always get the ids xD
-const gen = document.getElementById("generate");
-const text = document.getElementById("text");
-//Define some variables
-var num = Math.floor(Math.random() * 999),
-ones = ['U', 'D', 'T', 'q', 'Q', 's', 'S', 'O', 'N'],
-ones2 = ['M', 'B', 'T', 'q', 'Q', 's', 'S', 'O', 'N'],
-tens = ['D', 'V', 'TR', 'QU', 'QI', 'Sx', 'Sp', 'Oc', 'No'],
-hundreds = ['Ce', 'Du', 'Te', 'Qr', 'Qn', 'Sc', 'St', 'Ot', 'Nn'],
-thousands = ['Mil', 'Dil', 'Til', 'qil', 'Qil', 'sil', 'Sil', 'Oil', 'Nil'],
-millions = ['Myr', 'Dyr', 'Tyr', 'qyr', 'Qyr', 'syr', 'Syr', 'Oyr', 'Nyr'],
-billions = ['Mic', 'Dic', 'Tic', 'qic', 'Qic', 'sic', 'Sic', 'Oic', 'Nic'],
-trllions = ['Nan', 'DNan', 'TNan', 'qNan', 'QNan', 'sNan', 'SNan', 'ONan', 'NNan'],
-quadrillions = ['Pic', 'DPic', 'TPic', 'qPic', 'QPic', 'sPic', 'SPic', 'OPic', 'NPic'],
-quintillions = ['Fem', 'DFem', 'TFem', 'qFem', 'QFem', 'sFem', 'SFem', 'OFem', 'NFem'],
-sextillions = ['Att', 'DAtt', 'TAtt', 'qAtt', 'QAtt', 'sAtt', 'SAtt', 'OAtt', 'NAtt'],
-septillions = ['Zep', 'DZep', 'TZep', 'qZep', 'QZep', 'sZep', 'SZep', 'OZep', 'NZep'],
-octillions = ['Yoc', 'DYoc', 'TYoc', 'qYoc', 'QYoc', 'sYoc', 'SYoc', 'OYoc', 'NYoc'],
-nonillions = ['Xon', 'DXon', 'TXon', 'qXon', 'QXon', 'sXon', 'SXon', 'OXon', 'NXon'],
-decillions = ['Myr', 'Dyr', 'Tyr', 'qyr', 'Qyr', 'syr', 'Syr', 'Oyr', 'Nyr'];
-//Functions
-function ran(i) {Math.floor(Math.random() * i)};
-function length(i) {i.length};
-function inner(i) {text.innerHTML = i};
 
+//Variables again
+var random = Math.floor(Math.random() * 2)
 //"if"
-if (ran = 1) {inner(ran(length(ones2)))};
+if (random = 1) {text.innerHTML = Math.floor(Math.random() * ones.length)};
+gen.onclick = random;
---
+++ tileNum/script.js
@@ -0,0 +1,21 @@
+//Tile number generator.
+//Always get the ids xD
+const gen = document.getElementById("generate");
+const text = document.getElementById("text");
+//Define some variables
+var num = Math.floor(Math.random() * 999),
+ones = ['U', 'D', 'T', 'q', 'Q', 's', 'S', 'O', 'N'],
+ones2 = ['M', 'B', 'T', 'q', 'Q', 's', 'S', 'O', 'N'],
+tens = ['D', 'V', 'TR', 'QU', 'QI', 'Sx', 'Sp', 'Oc', 'No'],
+hundreds = ['Ce', 'Du', 'Te', 'Qr', 'Qn', 'Sc', 'St', 'Ot', 'Nn'],
+thousands = ['Mil', 'Dil', 'Til', 'qil', 'Qil', 'sil', 'Sil', 'Oil', 'Nil'],
+millions = ['Myr', 'Dyr', 'Tyr', 'qyr', 'Qyr', 'syr', 'Syr', 'Oyr', 'Nyr'],
+billions = ['Mic', 'Dic', 'Tic', 'qic', 'Qic', 'sic', 'Sic', 'Oic', 'Nic'],
+trllions = ['Nan', 'DNan', 'TNan', 'qNan', 'QNan', 'sNan', 'SNan', 'ONan', 'NNan'],
+quadrillions = ['Pic', 'DPic', 'TPic', 'qPic', 'QPic', 'sPic', 'SPic', 'OPic', 'NPic'],
+quintillions = ['Fem', 'DFem', 'TFem', 'qFem', 'QFem', 'sFem', 'SFem', 'OFem', 'NFem'],
+sextillions = ['Att', 'DAtt', 'TAtt', 'qAtt', 'QAtt', 'sAtt', 'SAtt', 'OAtt', 'NAtt'],
+septillions = ['Zep', 'DZep', 'TZep', 'qZep', 'QZep', 'sZep', 'SZep', 'OZep', 'NZep'],
+octillions = ['Yoc', 'DYoc', 'TYoc', 'qYoc', 'QYoc', 'sYoc', 'SYoc', 'OYoc', 'NYoc'],
+nonillions = ['Xon', 'DXon', 'TXon', 'qXon', 'QXon', 'sXon', 'SXon', 'OXon', 'NXon'],
+decillions = ['Myr', 'Dyr', 'Tyr', 'qyr', 'Qyr', 'syr', 'Syr', 'Oyr', 'Nyr'];
