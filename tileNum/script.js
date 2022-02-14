126fca0ae0108a7de26cad9b525a7eb68acbf714
Glitch (marcusicoi) <none>
2022-02-14 10:48:43 +0000

    Rewound to commit 6c2c4896f18891240f2a368bb994009611e721f1 + 0 edits

--- tileNum/script.js
+++ tileNum/script.js
@@ -1,44 +1,44 @@
 //Tile number generator.
 //Always get the ids xD
 const gen = document.getElementById('generate');
-const text = document.getElementById('text'); text.innerHTML = 1;
+const text = document.getElementById('text');
 //Define some variables
-var ones = ['', 'U', 'D', 'T', 'q', 'Q', 's', 'S', 'O', 'N'],
-ones2 = ['', 'M', 'D', 'T', 'q', 'Q', 's', 'S', 'O', 'N'],
-tens = ['', 'D', 'V', 'TR', 'QU', 'QI', 'Sx', 'Sp', 'Oc', 'No'],
-hundreds = ['', 'Ce', 'Du', 'Te', 'Qr', 'Qn', 'Sc', 'St', 'Ot', 'Nn'],
-thousands = ['', 'Mil', 'Dil', 'Til', 'qil', 'Qil', 'sil', 'Sil', 'Oil', 'Nil'],
-millions = ['', 'Myr', 'Dyr', 'Tyr', 'qyr', 'Qyr', 'syr', 'Syr', 'Oyr', 'Nyr'],
-billions = ['', 'Mic', 'Dic', 'Tic', 'qic', 'Qic', 'sic', 'Sic', 'Oic', 'Nic'],
-trillions = ['', 'Nan', 'DNan', 'TNan', 'qNan', 'QNan', 'sNan', 'SNan', 'ONan', 'NNan'],
-quadrillions = ['', 'Pic', 'DPic', 'TPic', 'qPic', 'QPic', 'sPic', 'SPic', 'OPic', 'NPic'],
-quintillions = ['', 'Fem', 'DFem', 'TFem', 'qFem', 'QFem', 'sFem', 'SFem', 'OFem', 'NFem'],
-sextillions = ['', 'Att', 'DAtt', 'TAtt', 'qAtt', 'QAtt', 'sAtt', 'SAtt', 'OAtt', 'NAtt'],
-septillions = ['', 'Zep', 'DZep', 'TZep', 'qZep', 'QZep', 'sZep', 'SZep', 'OZep', 'NZep'],
-octillions = ['', 'Yoc', 'DYoc', 'TYoc', 'qYoc', 'QYoc', 'sYoc', 'SYoc', 'OYoc', 'NYoc'],
-nonillions = ['', 'Xon', 'DXon', 'TXon', 'qXon', 'QXon', 'sXon', 'SXon', 'OXon', 'NXon'],
-decillions = ['', 'Vec', 'DVec', 'TVec', 'qVec', 'QVec', 'sVec', 'SVec', 'OVec', 'NVec'],
-undecillions = ['', 'Mec', 'DMec', 'TMec', 'qMec', 'QMec', 'sMec', 'SMec', 'OMec', 'NMec'],
-duodecillions = ['', 'Duec', 'DDuec', 'TDuec', 'qDuec', 'QDuec', 'sDuec', 'SDuec', 'ODuec', 'NDuec'],
-tredecillions = ['', 'Trec', 'DTrec', 'TTrec', 'qTrec', 'QTrec', 'sTrec', 'STrec', 'OTrec', 'NTrec'],
-quattuordecillions = ['', 'Tetc', 'DTetc', 'TTetc', 'qTetc', 'QTetc', 'sTetc', 'STetc', 'OTetc', 'NTetc'],
-quintdecillions = ['', 'Penc', 'DPenc', 'TPenc', 'qPenc', 'QPenc', 'sPenc', 'SPenc', 'OPenc', 'NPenc'],
-sexdecillions = ['', 'Hexc', 'DHexc', 'THexc', 'qHexc', 'QHexc', 'sHexc', 'SHexc', 'OHexc', 'NHexc'],
-septemdecillions = ['', 'Hepc', 'DHepc', 'THepc', 'qHepc', 'QHepc', 'sHepc', 'SHepc', 'OHepc', 'NHepc'],
-octodecillions = ['', 'Octc', 'DOctc', 'TOctc', 'qOctc', 'QOctc', 'sOctc', 'SOctc', 'OOctc', 'NOctc'],
-novemdecillions = ['', 'Ennc', 'DEnnc', 'TEnnc', 'qEnnc', 'QEnnc', 'sEnnc', 'SEnnc', 'OEnnc', 'NEnnc'],
-vigintillions = ['', 'Icos', 'DIcos', 'TIcos', 'qIcos', 'QIcos', 'sIcos', 'SIcos', 'OIcos', 'NIcos'],
-unvigintillions = ['', 'Dico', 'DDico', 'TDico', 'qDico', 'QDico', 'sDico', 'SDico', 'ODico', 'NDico'],
-duovigintillions = ['', 'Trio', 'DTrio', 'TTrio', 'qTrio', 'QTrio', 'sTrio', 'STrio', 'OTrio', 'NTrio'],
-trevigintillions = ['', 'Teto', 'DTeto', 'TTeto', 'qTeto', 'QTeto', 'sTeto', 'STeto', 'OTeto', 'NTeto'],
-quattuorvigintillions = ['', 'Peno', 'DPeno', 'TPeno', 'qPeno', 'QPeno', 'sPeno', 'SPeno', 'OPeno', 'NPeno'],
-quintvigintillions = ['', 'Hexo', 'DHexo', 'THexo', 'qHexo', 'QHexo', 'sHexo', 'SHexo', 'OHexo', 'NHexo'],
-sexvigintillions = ['', 'Hepto', 'DHepto', 'THepto', 'qHepto', 'QHepto', 'sHepto', 'SHepto', 'OHepto', 'NHepto'],
-septemvigintillions = ['', 'Octic', 'DOctic', 'TOctic', 'qOctic', 'QOctic', 'sOctic', 'SOctic', 'OOctic', 'NOctic'],
-octovigintillions = ['', 'Ennic', 'DEnnic', 'TEnnic', 'qEnnic', 'QEnnic', 'sEnnic', 'SEnnic', 'OEnnic', 'NEnnic'],
-novemvigintillions = ['', 'Trico', 'DTrico', 'TTrico', 'qTrico', 'QTrico', 'sTrico', 'STrico', 'OTrico', 'NTrico'],
-trigintillions = ['', 'Metco', 'DMetco', 'TMetco', 'qMetco', 'QMecto', 'sMecto', 'SMecto', 'OMecto', 'NMecto'],
-untrigintillions = ['', 'Dueco', 'DDueco', 'TDueco', 'qDueco', 'QDueco', 'sDueco', 'SDueco', 'ODueco', 'NDueco'];
+var ones = ['U', 'D', 'T', 'q', 'Q', 's', 'S', 'O', 'N'],
+ones2 = ['M', 'D', 'T', 'q', 'Q', 's', 'S', 'O', 'N'],
+tens = ['D', 'V', 'TR', 'QU', 'QI', 'Sx', 'Sp', 'Oc', 'No'],
+hundreds = ['Ce', 'Du', 'Te', 'Qr', 'Qn', 'Sc', 'St', 'Ot', 'Nn'],
+thousands = ['Mil', 'Dil', 'Til', 'qil', 'Qil', 'sil', 'Sil', 'Oil', 'Nil'],
+millions = ['Myr', 'Dyr', 'Tyr', 'qyr', 'Qyr', 'syr', 'Syr', 'Oyr', 'Nyr'],
+billions = ['Mic', 'Dic', 'Tic', 'qic', 'Qic', 'sic', 'Sic', 'Oic', 'Nic'],
+trillions = ['Nan', 'DNan', 'TNan', 'qNan', 'QNan', 'sNan', 'SNan', 'ONan', 'NNan'],
+quadrillions = ['Pic', 'DPic', 'TPic', 'qPic', 'QPic', 'sPic', 'SPic', 'OPic', 'NPic'],
+quintillions = ['Fem', 'DFem', 'TFem', 'qFem', 'QFem', 'sFem', 'SFem', 'OFem', 'NFem'],
+sextillions = ['Att', 'DAtt', 'TAtt', 'qAtt', 'QAtt', 'sAtt', 'SAtt', 'OAtt', 'NAtt'],
+septillions = ['Zep', 'DZep', 'TZep', 'qZep', 'QZep', 'sZep', 'SZep', 'OZep', 'NZep'],
+octillions = ['Yoc', 'DYoc', 'TYoc', 'qYoc', 'QYoc', 'sYoc', 'SYoc', 'OYoc', 'NYoc'],
+nonillions = ['Xon', 'DXon', 'TXon', 'qXon', 'QXon', 'sXon', 'SXon', 'OXon', 'NXon'],
+decillions = ['Vec', 'DVec', 'TVec', 'qVec', 'QVec', 'sVec', 'SVec', 'OVec', 'NVec'],
+undecillions = ['Mec', 'DMec', 'TMec', 'qMec', 'QMec', 'sMec', 'SMec', 'OMec', 'NMec'],
+duodecillions = ['Duec', 'DDuec', 'TDuec', 'qDuec', 'QDuec', 'sDuec', 'SDuec', 'ODuec', 'NDuec'],
+tredecillions = ['Trec', 'DTrec', 'TTrec', 'qTrec', 'QTrec', 'sTrec', 'STrec', 'OTrec', 'NTrec'],
+quattuordecillions = ['Tetc', 'DTetc', 'TTetc', 'qTetc', 'QTetc', 'sTetc', 'STetc', 'OTetc', 'NTetc'],
+quintdecillions = ['Penc', 'DPenc', 'TPenc', 'qPenc', 'QPenc', 'sPenc', 'SPenc', 'OPenc', 'NPenc'],
+sexdecillions = ['Hexc', 'DHexc', 'THexc', 'qHexc', 'QHexc', 'sHexc', 'SHexc', 'OHexc', 'NHexc'],
+septemdecillions = ['Hepc', 'DHepc', 'THepc', 'qHepc', 'QHepc', 'sHepc', 'SHepc', 'OHepc', 'NHepc'],
+octodecillions = ['Octc', 'DOctc', 'TOctc', 'qOctc', 'QOctc', 'sOctc', 'SOctc', 'OOctc', 'NOctc'],
+novemdecillions = ['Ennc', 'DEnnc', 'TEnnc', 'qEnnc', 'QEnnc', 'sEnnc', 'SEnnc', 'OEnnc', 'NEnnc'],
+vigintillions = ['Icos', 'DIcos', 'TIcos', 'qIcos', 'QIcos', 'sIcos', 'SIcos', 'OIcos', 'NIcos'],
+unvigintillions = ['Dico', 'DDico', 'TDico', 'qDico', 'QDico', 'sDico', 'SDico', 'ODico', 'NDico'],
+duovigintillions = ['Trio', 'DTrio', 'TTrio', 'qTrio', 'QTrio', 'sTrio', 'STrio', 'OTrio', 'NTrio'],
+trevigintillions = ['Teto', 'DTeto', 'TTeto', 'qTeto', 'QTeto', 'sTeto', 'STeto', 'OTeto', 'NTeto'],
+quattuorvigintillions = ['Peno', 'DPeno', 'TPeno', 'qPeno', 'QPeno', 'sPeno', 'SPeno', 'OPeno', 'NPeno'],
+quintvigintillions = ['Hexo', 'DHexo', 'THexo', 'qHexo', 'QHexo', 'sHexo', 'SHexo', 'OHexo', 'NHexo'],
+sexvigintillions = ['Hepto', 'DHepto', 'THepto', 'qHepto', 'QHepto', 'sHepto', 'SHepto', 'OHepto', 'NHepto'],
+septemvigintillions = ['Octic', 'DOctic', 'TOctic', 'qOctic', 'QOctic', 'sOctic', 'SOctic', 'OOctic', 'NOctic'],
+octovigintillions = ['Ennic', 'DEnnic', 'TEnnic', 'qEnnic', 'QEnnic', 'sEnnic', 'SEnnic', 'OEnnic', 'NEnnic'],
+novemvigintillions = ['Trico', 'DTrico', 'TTrico', 'qTrico', 'QTrico', 'sTrico', 'STrico', 'OTrico', 'NTrico'],
+trigintillions = ['Metco', 'DMetco', 'TMetco', 'qMetco', 'QMecto', 'sMecto', 'SMecto', 'OMecto', 'NMecto'],
+untrigintillions = ['Dueco', 'DDueco', 'TDueco', 'qDueco', 'QDueco', 'sDueco', 'SDueco', 'ODueco', 'NDueco'];
 
 function textResult() {
 //Random Variables
@@ -79,7 +79,6 @@ ov = octovigintillions[Math.floor(Math.random() * octovigintillions.length)],
 nv = novemvigintillions[Math.floor(Math.random() * novemvigintillions.length)],
 tr = trigintillions[Math.floor(Math.random() * trigintillions.length)],
 utr = untrigintillions[Math.floor(Math.random() * untrigintillions.length)];
-let num2 = 1;
-text.innerHTML = (text.innerHTML * 2);
+text.innerHTML = num + utr + tr + nv + ov + Sv + sv + Qv + qv + tv + dv + uv + v + nd + od + Sd + sd + Qd + qd + td + dd + ud + d + n + O + S + s + Q + q + tri + b + m + k + o + t + h;
 }
 gen.onclick = function() {textResult();};
