955c370760b2599d8a190c9398d87dff763855d2
Glitch (prickle-woodpecker) <none>
2022-02-14 01:01:30 +0000

    📅🍯 Checkpoint ./tileNum/script.js:27704817/105 ./tileNum/index.html:27704817/235

--- tileNum/script.js
+++ tileNum/script.js
@@ -11,7 +11,7 @@ hundreds = ['Ce', 'Du', 'Te', 'Qr', 'Qn', 'Sc', 'St', 'Ot', 'Nn'],
 thousands = ['Mil', 'Dil', 'Til', 'qil', 'Qil', 'sil', 'Sil', 'Oil', 'Nil'],
 millions = ['Myr', 'Dyr', 'Tyr', 'qyr', 'Qyr', 'syr', 'Syr', 'Oyr', 'Nyr'],
 billions = ['Mic', 'Dic', 'Tic', 'qic', 'Qic', 'sic', 'Sic', 'Oic', 'Nic'],
-trllions = ['Nan', 'DNan', 'TNan', 'qNan', 'QNan', 'sNan', 'SNan', 'ONan', 'NNan'],
+trillions = ['Nan', 'DNan', 'TNan', 'qNan', 'QNan', 'sNan', 'SNan', 'ONan', 'NNan'],
 quadrillions = ['Pic', 'DPic', 'TPic', 'qPic', 'QPic', 'sPic', 'SPic', 'OPic', 'NPic'],
 quintillions = ['Fem', 'DFem', 'TFem', 'qFem', 'QFem', 'sFem', 'SFem', 'OFem', 'NFem'],
 sextillions = ['Att', 'DAtt', 'TAtt', 'qAtt', 'QAtt', 'sAtt', 'SAtt', 'OAtt', 'NAtt'],
@@ -21,14 +21,15 @@ nonillions = ['Xon', 'DXon', 'TXon', 'qXon', 'QXon', 'sXon', 'SXon', 'OXon', 'NX
 decillions = ['Myr', 'Dyr', 'Tyr', 'qyr', 'Qyr', 'syr', 'Syr', 'Oyr', 'Nyr'];
 function textResult() {
 //Random Variables
-var random = Math.floor(Math.random() * 64),
+var random = Math.floor(Math.random() * 127),
 o2ran = ones2[Math.floor(Math.random() * ones2.length)],
 oran = ones[Math.floor(Math.random() * ones.length)],
 tran = tens[Math.floor(Math.random() * tens.length)],
 hran = hundreds[Math.floor(Math.random() * hundreds.length)],
 t2ran = thousands[Math.floor(Math.random() * thousands.length)],
 mran = millions[Math.floor(Math.random() * millions.length)],
-bran = billions[Math.floor(Math.random() * billions.length)];
+bran = billions[Math.floor(Math.random() * billions.length)],
+t3ran = trillions[Math.floor(Math.random() * trillions.length)];
 //"if"
 if (random === 1) {text.innerHTML = num};
 if (random === 2) {text.innerHTML = num + o2ran};
