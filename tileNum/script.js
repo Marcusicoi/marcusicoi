cba41ee08e7d6acbdd1e4f587309795de0f2efe5
Glitch (prickle-woodpecker) <none>
2022-02-14 04:31:47 +0000

    🍼🏭 Checkpoint ./tileNum/script.js:27704817/10418

--- tileNum/script.js
+++ tileNum/script.js
@@ -1,7 +1,7 @@
 //Tile number generator.
 //Always get the ids xD
 const gen = document.getElementById('generate');
-const gen2 = document.getElementById('generate2').value;
+const gen2 = document.getElementById('generate2');
 const but = document.getElementById('button');
 const text = document.getElementById('text');
 //Define some variables
@@ -21,10 +21,10 @@ septillions = ['Zep', 'DZep', 'TZep', 'qZep', 'QZep', 'sZep', 'SZep', 'OZep', 'N
 octillions = ['Yoc', 'DYoc', 'TYoc', 'qYoc', 'QYoc', 'sYoc', 'SYoc', 'OYoc', 'NYoc'],
 nonillions = ['Xon', 'DXon', 'TXon', 'qXon', 'QXon', 'sXon', 'SXon', 'OXon', 'NXon'],
 decillions = ['Vec', 'DVec', 'TVec', 'qVec', 'QVec', 'sVec', 'SVec', 'OVec', 'NVec'];
-function textResult() {
+
+function textResult2() {
 //Random Variables
-var random = Math.floor(Math.random() * 65),
-o2ran = ones2[Math.floor(Math.random() * ones2.length)],
+var o2ran = ones2[Math.floor(Math.random() * ones2.length)],
 oran = ones[Math.floor(Math.random() * ones.length)],
 tran = tens[Math.floor(Math.random() * tens.length)],
 hran = hundreds[Math.floor(Math.random() * hundreds.length)],
@@ -39,71 +39,7 @@ s2ran = septillions[Math.floor(Math.random() *  septillions.length)],
 o3ran = octillions[Math.floor(Math.random() * octillions.length)],
 nran = nonillions[Math.floor(Math.random() * nonillions.length)],
 dran = decillions[Math.floor(Math.random() * decillions.length)];
-//"if"
-if (random === 1) {text.innerHTML = num};
-if (random === 2) {text.innerHTML = num + o2ran};
-if (random === 3) {text.innerHTML = num + tran};
-if (random === 4) {text.innerHTML = num + oran + tran};
-if (random === 5) {text.innerHTML = num + hran};;
-if (random === 6) {text.innerHTML = num + oran + hran};
-if (random === 7) {text.innerHTML = num + tran + hran};
-if (random === 8) {text.innerHTML = num + oran + tran + hran};
-if (random === 9) {text.innerHTML = num + t2ran};
-if (random === 10) {text.innerHTML = num + t2ran + oran};
-if (random === 11) {text.innerHTML = num + t2ran + tran};
-if (random === 12) {text.innerHTML = num + t2ran + oran + tran};
-if (random === 13) {text.innerHTML = num + t2ran + hran};
-if (random === 14) {text.innerHTML = num + t2ran + oran + hran};
-if (random === 15) {text.innerHTML = num + t2ran + tran + hran};
-if (random === 16) {text.innerHTML = num + t2ran + oran + tran + hran};
-if (random === 17) {text.innerHTML = num + mran};
-if (random === 18) {text.innerHTML = num + mran + oran};
-if (random === 19) {text.innerHTML = num + mran + tran};
-if (random === 20) {text.innerHTML = num + mran + oran + tran};
-if (random === 21) {text.innerHTML = num + mran + hran};
-if (random === 22) {text.innerHTML = num + mran + oran + hran};
-if (random === 23) {text.innerHTML = num + mran + tran + hran};
-if (random === 24) {text.innerHTML = num + mran + oran + tran + hran};
-if (random === 25) {text.innerHTML = num + mran + t2ran};
-if (random === 26) {text.innerHTML = num + mran + t2ran + oran};
-if (random === 27) {text.innerHTML = num + mran + t2ran + tran};
-if (random === 28) {text.innerHTML = num + mran + t2ran + oran + tran};
-if (random === 29) {text.innerHTML = num + mran + t2ran + hran};
-if (random === 30) {text.innerHTML = num + mran + t2ran + oran + hran};
-if (random === 31) {text.innerHTML = num + mran + t2ran + tran + hran};
-if (random === 32) {text.innerHTML = num + mran + t2ran + oran + tran + hran};
-if (random === 33) {text.innerHTML = num + bran + oran};
-if (random === 34) {text.innerHTML = num + bran + tran};
-if (random === 35) {text.innerHTML = num + bran + oran + tran};
-if (random === 36) {text.innerHTML = num + bran + hran};;
-if (random === 37) {text.innerHTML = num + bran + oran + hran};
-if (random === 38) {text.innerHTML = num + bran + tran + hran};
-if (random === 39) {text.innerHTML = num + bran + oran + tran + hran};
-if (random === 40) {text.innerHTML = num + bran + t2ran};
-if (random === 41) {text.innerHTML = num + bran + t2ran + oran};
-if (random === 42) {text.innerHTML = num + bran + t2ran + tran};
-if (random === 43) {text.innerHTML = num + bran + t2ran + oran + tran};
-if (random === 44) {text.innerHTML = num + bran + t2ran + hran};
-if (random === 45) {text.innerHTML = num + bran + t2ran + oran + hran};
-if (random === 46) {text.innerHTML = num + bran + t2ran + tran + hran};
-if (random === 47) {text.innerHTML = num + bran + t2ran + oran + tran + hran};
-if (random === 48) {text.innerHTML = num + bran + mran};
-if (random === 49) {text.innerHTML = num + bran + mran + oran};
-if (random === 50) {text.innerHTML = num + bran + mran + tran};
-if (random === 51) {text.innerHTML = num + bran + mran + oran + tran};
-if (random === 52) {text.innerHTML = num + bran + mran + hran};
-if (random === 53) {text.innerHTML = num + bran + mran + oran + hran};
-if (random === 54) {text.innerHTML = num + bran + mran + tran + hran};
-if (random === 55) {text.innerHTML = num + bran + mran + oran + tran + hran};
-if (random === 56) {text.innerHTML = num + bran + mran + t2ran};
-if (random === 57) {text.innerHTML = num + bran + mran + t2ran + oran};
-if (random === 58) {text.innerHTML = num + bran + mran + t2ran + tran};
-if (random === 59) {text.innerHTML = num + bran + mran + t2ran + oran + tran};
-if (random === 60) {text.innerHTML = num + bran + mran + t2ran + hran};
-if (random === 61) {text.innerHTML = num + bran + mran + t2ran + oran + hran};
-if (random === 62) {text.innerHTML = num + bran + mran + t2ran + tran + hran};
-if (random === 63) {text.innerHTML = num + bran + mran + t2ran + oran + tran + hran};
+//"if" As Always.
+if (gen2.value === "O") {text.innerHTML = num + o2ran};
 }
-gen.onclick = function() {textResult()};
-function textResult2() {
-if (gen2 === "arras") {text.innerHTML = 
+but.onclick = function() {textResult2();};
