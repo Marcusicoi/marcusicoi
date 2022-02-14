233330ff574e0213af9b39f023ee17db5d2685fd
Glitch (marcusicoi) <none>
2022-02-14 05:08:06 +0000

    🎁💐 Checkpoint ./tileNum/script.js:27704817/1544 ./tileNum/index.html:27704817/83

--- tileNum/script.js
+++ tileNum/script.js
@@ -1,11 +1,10 @@
 //Tile number generator.
 //Always get the ids xD
 const gen = document.getElementById('generate');
-const but = document.getElementById('button');
 const text = document.getElementById('text');
 //Define some variables
 var ones = ['U', 'D', 'T', 'q', 'Q', 's', 'S', 'O', 'N'],
-ones2 = ['M', 'B', 'T', 'q', 'Q', 's', 'S', 'O', 'N'],
+ones2 = ['M', 'D', 'T', 'q', 'Q', 's', 'S', 'O', 'N'],
 tens = ['D', 'V', 'TR', 'QU', 'QI', 'Sx', 'Sp', 'Oc', 'No'],
 hundreds = ['Ce', 'Du', 'Te', 'Qr', 'Qn', 'Sc', 'St', 'Ot', 'Nn'],
 thousands = ['Mil', 'Dil', 'Til', 'qil', 'Qil', 'sil', 'Sil', 'Oil', 'Nil'],
@@ -18,9 +17,12 @@ sextillions = ['Att', 'DAtt', 'TAtt', 'qAtt', 'QAtt', 'sAtt', 'SAtt', 'OAtt', 'N
 septillions = ['Zep', 'DZep', 'TZep', 'qZep', 'QZep', 'sZep', 'SZep', 'OZep', 'NZep'],
 octillions = ['Yoc', 'DYoc', 'TYoc', 'qYoc', 'QYoc', 'sYoc', 'SYoc', 'OYoc', 'NYoc'],
 nonillions = ['Xon', 'DXon', 'TXon', 'qXon', 'QXon', 'sXon', 'SXon', 'OXon', 'NXon'],
-decillions = ['Vec', 'DVec', 'TVec', 'qVec', 'QVec', 'sVec', 'SVec', 'OVec', 'NVec'];
+decillions = ['Vec', 'DVec', 'TVec', 'qVec', 'QVec', 'sVec', 'SVec', 'OVec', 'NVec'],
+undecillions = ['Mec', 'DMec', 'TMec', 'qMec', 'QMec', 'sMec', 'SMec', 'OMec', 'NMec'],
+duodecillions = ['Duec', 'DDuec', 'TDuec', 'qDuec', 'QDuec', 'sDuec', 'SDuec', 'ODuec', 'NDuec'],
+tredecillions = ['M', 'D', 'T', 'q', 'Q', 's', 'S', 'O', 'N'],
 
-function textResult2() {
+function textResult() {
 //Random Variables
 var num = Math.floor(Math.random() * 999),
 o2ran = ones2[Math.floor(Math.random() * ones2.length)],
@@ -38,21 +40,6 @@ s2ran = septillions[Math.floor(Math.random() *  septillions.length)],
 o3ran = octillions[Math.floor(Math.random() * octillions.length)],
 nran = nonillions[Math.floor(Math.random() * nonillions.length)],
 dran = decillions[Math.floor(Math.random() * decillions.length)];
-//"if" As Always.
-if (gen.value === "O") {text.innerHTML = num + o2ran};
-if (gen.value === "T") {text.innerHTML = num + tran};
-if (gen.value === "OT") {text.innerHTML = num + oran + tran};
-if (gen.value === "H") {text.innerHTML = num + hran};
-if (gen.value === "OH") {text.innerHTML = num + oran + hran};
-if (gen.value === "TH") {text.innerHTML = num + tran + hran};
-if (gen.value === "OTH") {text.innerHTML = num + oran + tran + hran};
-if (gen.value === "K") {text.innerHTML = num + t2ran};
-if (gen.value === "KO") {text.innerHTML = num + t2ran + o2ran};
-if (gen.value === "KT") {text.innerHTML = num + t2ran + tran};
-if (gen.value === "KOT") {text.innerHTML = num + t2ran + oran + tran};
-if (gen.value === "KH") {text.innerHTML = num + t2ran + hran};
-if (gen.value === "KOH") {text.innerHTML = num + t2ran + oran + hran};
-if (gen.value === "KTH") {text.innerHTML = num + t2ran + tran + hran};
-if (gen.value === "KOTH") {text.innerHTML = num + t2ran + oran + tran + hran};
+text.innerHTML = num + dran + nran + oran + s2ran + sran + q2ran + qran + t3ran + bran + mran + t2ran + oran + tran + hran;
 }
-but.onclick = function() {textResult2();};
+gen.onclick = function() {textResult();};
