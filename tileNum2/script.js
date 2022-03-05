545598bb03147a12f612088ffa33e9574a73de83
Glitch (marcusicoi) <none>
2022-03-05 03:51:23 +0000

    ⛺️💖 Checkpoint ./tileNum2/script.js:27704817/6957

--- tileNum2/script.js
+++ tileNum2/script.js
@@ -16,7 +16,7 @@
       }
     };
     //Stop increasing
-    function stopStopwatch() {
+    function stopIncreasing() {
       if (stopincrease == false) {
           stopincrease = true;
       }
@@ -28,144 +28,36 @@
         num += 1;
         //Number calculations
         if (num > 10) {
-            spd = 800
+            spd = 900
         }
         if (num > 100) {
-            spd = 600
+            spd = 800
         }
         if (num = 1000) {
             num = num + "K";
             num = 0;
-            spd = 200;
+            spd = 700;
         };
-        if (min == 60) {
-            hr += 1;
-            min = 0;
-            sec = 0;
+        if (num = 1000 + "K") {
+            num = num + "M";
+            num = 0;
+            spd = 600;
         };
-        if (hr == 24) {
-            dy += 1;
-            hr = 0;
-            min = 0;
-            sec = 0;
+        if (num = 1000 + "M") {
+            num = num + "B";
+            num = 0;
+            spd = 500;
         }
-        if (dy == 7) {
-            wk += 1;
-            dy = 0;
-            hr = 0;
-            min = 0;
-            sec = 0;
-        }
-        if (wk > 4.34524) {
-            mn += 1;
-            wk = 0;
-            dy = 0;
-            hr = 0;
-            min = 0;
-            sec = 0;
-        }
-        //Set the timeout and change the timer by every second
-        timer.getElementsByClassName('months')[0].innerHTML = mn;
-        timer.getElementsByClassName('weeks')[0].innerHTML = wk;
-        timer.getElementsByClassName('days')[0].innerHTML = dy;
-        timer.getElementsByClassName('hours')[0].innerHTML = hr;
-        timer.getElementsByClassName('minutes')[0].innerHTML = min;
-        timer.getElementsByClassName('seconds')[0].innerHTML = sec; 
-        setTimeout("stopwatchCycle()", 1000);
+        //Set the timeout and change the number by every second based on the spd
+        number.innerHTML = num; 
+        setTimeout("numberCycle()", spd);
     };
    }
-//Reset the stopwatch
-function resetStopwatch() {
-    timer.getElementsByClassName('months')[0].innerHTML = '0';
-    timer.getElementsByClassName('weeks')[0].innerHTML = '0';
-    timer.getElementsByClassName('days')[0].innerHTML = '0';
-    timer.getElementsByClassName('hours')[0].innerHTML = '0';
-    timer.getElementsByClassName('minutes')[0].innerHTML = '0';
-    timer.getElementsByClassName('seconds')[0].innerHTML = '0';
-    stoptime = true;
-    sec = 0; min = 0; hr = 0; dy = 0; wk = 0; mn = 0;
-};/*
-function randomColors() {
-let colors = ['red', 'orange', 'yellow', '#ccff00', 'lime',
-              '##00ffaa', 'cyan', '#00aaff', 'blue', 'purple',
-              'pink', '#ff0077'];
- let number = Math.floor(Math.random() * colors.length);
- let random = colors[number];
- let number2 = Math.floor(Math.random() * colors.length);
- let random2 = colors[number2];
- let number3 = Math.floor(Math.random() * colors.length);
- let random3 = colors[number3];
- let number4 = Math.floor(Math.random() * colors.length);
- let random4 = colors[number4];
- timer.getElementsByClassName('day')[0].style.color = 'white'
- timer.getElementsByClassName('day')[0].style.background = random;
- timer.getElementsByClassName('hour')[0].style.color = 'white'
- timer.getElementsByClassName('hour')[0].style.background = random2;
- timer.getElementsByClassName('minute')[0].style.color = 'white'
- timer.getElementsByClassName('minute')[0].style.background = random3;
- timer.getElementsByClassName('second')[0].style.color = 'white'
- timer.getElementsByClassName('second')[0].style.background = random4;
-};*/
-//Lets make the "timed" class cooler and lets start with gradients
-function randomGradient() {
- //All Gradients. the // part was the gradient name
- let gradients = ['linear-gradient(to right, #ffafbd, #ffc3a0)', //Roseannna
-                  'linear-gradient(to right, #2193b0, #6dd5ed)', //Sexy Blue
-                  'linear-gradient(to right, #cc2b5e, #753a88)', //Purple Love
-                  'linear-gradient(to right, #ee9ca7, #ffdde1)', //Piglet
-                  'linear-gradient(to right, #42275a, #734b6d)', //Mauve
-                  'linear-gradient(to right, #bdc3c7, #2c3e50)', //50 Shades of gray
-                  'linear-gradient(to right, #de6262, #ffb88c)', //A Lost Memory
-                  'linear-gradient(to right, #06beb6, #48b1bf)', //Socialive
-                  'linear-gradient(to right, #eb3349, #f45c43)', //Cherry
-                  'linear-gradient(to right, #dd5e89, #f7bb97)', //Pinky
-                  'linear-gradient(to right, #56ab2f, #a8e063)', //Lush
-                  'linear-gradient(to right, #614385, #516395)', //Kashmir
-                  'linear-gradient(to right, #eecda3, #ef629f)', //Tranquil
-                  'linear-gradient(to right, #eacda3, #d6ae7b)', //Pale Wood
-           'linear-gradient(to bottom right, #02aab0, #00cdac)', //Green Beach
-                  'linear-gradient(to right, #d66d75, #e29587)', //Sha La La
-                  'linear-gradient(to right, #000428, #004e92)', //Frost
-                  'linear-gradient(to right, #ddd6fe, #faaca8)', //Almost
-                  'linear-gradient(to right, #7b4397, #dc2430)', //Virgin America
-                  'linear-gradient(to right, #43cea2, #185a9d)', //Endless River
-                  'linear-gradient(to right, #ba5370, #f4e2d8)', //Purple White
-                  'linear-gradient(to right, #ff512f, #dd2476)', //Bloody Mary
-                  'linear-gradient(to right, #4568dc, #b06ab3)', //Can you feel the love tonight
-                  'linear-gradient(to right, #ec6f66, #f3a183)', //Bourbon
-                  'linear-gradient(to right, #ffd89b, #19547b)', //Dusk
-         'linear-gradient(to right, #3a1c71, #d76d77, #ffaf7b)', //Relay
-                  'linear-gradient(to right, #4ca1af, #c4e0e5)', //Decent
-                  'linear-gradient(to right, #ff5f6d, #ffce71)', //Sweet Morning
-                  'linear-gradient(to right, #36d1dc, #5b86e5)', //Scooter
-                  'linear-gradient(to right, #c33764, #1d2671)', //Celestial
-                  'linear-gradient(to right, #141e30, #243b55)', //Royal
-                  'linear-gradient(to right, #ff7e5f, #feb47b)', //Ed's Sunset Gradient
-                  'linear-gradient(to right, #ed4264, #ffedbc)', //Peach
-                  'linear-gradient(to right, #2b5876, #4e4376)', //Sea Blue
-                  'linear-gradient(to right, #ff9966, #ff5e62)', //Orange Coral
-                  'linear-gradient(to right, #aa076b, #61045f)', //Aubergine
-                  ];
- //Randomize them (i have to duplicate because it returns the same gradient
- let num = Math.floor(Math.random() * gradients.length);
- let ran = gradients[num];
- let num2 = Math.floor(Math.random() * gradients.length);
- let ran2 = gradients[num2];
- let num3 = Math.floor(Math.random() * gradients.length);
- let ran3 = gradients[num3];
- let num4 = Math.floor(Math.random() * gradients.length);
- let ran4 = gradients[num4];
- let num5 = Math.floor(Math.random() * gradients.length);
- let ran5 = gradients[num5];
- let num6 = Math.floor(Math.random() * gradients.length);
- let ran6 = gradients[num6];
- //Use the gradient randomizer to classes
- timer.getElementsByClassName('month')[0].style.background = ran6;
- timer.getElementsByClassName('week')[0].style.background = ran5;
- timer.getElementsByClassName('day')[0].style.background = ran4;
- timer.getElementsByClassName('hour')[0].style.background = ran3;
- timer.getElementsByClassName('minute')[0].style.background = ran2;
- timer.getElementsByClassName('second')[0].style.background = ran;
+//Reset the number
+function resetNumber() {
+ stopincrease = true;
+ number.innerHTML = 0;
+ num = 0;
 };
 //Make the two buttons invisible
 stop.style.visibility = 'hidden';
@@ -175,21 +67,18 @@ start.onclick = function() {
   stop.style.visibility = 'visible';
   reset.style.visibility = 'visible';
   start.style.visibility = 'hidden';
-  startStopwatch();
+  startIncreasing();
 };
 stop.onclick = function() {
   if (start.innerHTML === 'Start') {start.innerHTML = 'Resume'};
   stop.style.visibility = 'hidden';
   start.style.visibility = 'visible';
-  stopStopwatch();
+  stopIncreasing();
 };
 reset.onclick = function() {
   if (start.innerHTML === 'Resume') {start.innerHTML = 'Start'}; 
   stop.style.visibility = 'hidden';
   reset.style.visibility = 'hidden';
   start.style.visibility = 'visible';
-  resetStopwatch();
+  resetNumber();
 }; 
-//Use the randomGradient function while the website is onload
-window.onload = function() {randomGradient()};
-
