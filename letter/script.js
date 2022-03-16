77bf27e6b16a365d4745d3e0be5bd4a3ecd12e43
Glitch (marcusicoi) <none>
2022-03-16 06:14:04 +0000

    😹🗻 Checkpoint ./letter/script.js:27704817/730

--- letter/script.js
+++ letter/script.js
@@ -2,3 +2,5 @@
 const text = document.getElementById("text");
 const gen = document.getElementById("button");
 //Second, Define the variables
+var vowels = ['a', 'e', 'i', 'o', 'u'],
+consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', '  
