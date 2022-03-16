a005ece6911e73ae626385fd24d285f57a1564b4
Glitch (marcusicoi) <none>
2022-03-16 06:26:03 +0000

    🏞🐂 Checkpoint ./letter/script.js:27704817/458

--- letter/script.js
+++ letter/script.js
@@ -1,6 +1,12 @@
-//First, always define the ids
+//First, Always define the ids.
 const text = document.getElementById("text");
 const gen = document.getElementById("button");
-//Second, Define the variables
+//Second, Define the variables.
 var vowels = ['a', 'e', 'i', 'o', 'u'],
-consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', '  
+consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'];  
+//Third, Functions.
+function Generate() {
+//Randomize the letters
+let ranv = vowels[Math.floor(Math.random() * vowels.length)],
+ranc = consonants[Math.floor(Math.random() * consonants.length)];.
+//Convert it
