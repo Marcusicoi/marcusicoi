1f43003a5d024084e808fdef23ecca1d8c9b8d6c
Glitch (marcusicoi) <none>
2022-03-16 11:35:40 +0000

    🛋💄 Checkpoint ./letter/script.js:27704817/446 ./letter/index.html:27704817/11

--- letter/script.js
+++ letter/script.js
@@ -1,6 +1,7 @@
 //First, Always define the ids.
 const text = document.getElementById("text");
 const gen = document.getElementById("button");
+const input = document.getElementById("input");
 //Second, Define the variables.
 var vowels = ['a', 'e', 'i', 'o', 'u'],
 consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'];  
@@ -11,6 +12,8 @@ let ranv = vowels[Math.floor(Math.random() * vowels.length)],
 ranc = consonants[Math.floor(Math.random() * consonants.length)],
 vandc = [vowels, consonants],
 letter = vandc[Math.floor(Math.random() * vandc.length)];
-//Text
-text.innerHTML = 
-//Convert it
+//Make another function that manages how many characters
+function Characters(id, value) {
+if (
+  
+  
