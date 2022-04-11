2e44be62e4d72d88c60995ad005b64e1d9a247b1
Glitch (marcusicoi) <none>
2022-04-11 13:53:52 +0000

    😬🔔 Checkpoint ./letter/script.js:27704817/427

--- letter/script.js
+++ letter/script.js
@@ -8,13 +8,18 @@ const word3 = document.getElementById("thirdword");
 const word4 = document.getElementById("fourthword");
 const word5 = document.getElementById("fifthword");
 //Second, define the variables.
-var numbers = [1, 2, 3, 4, 5];
-var ran1 = numbers[Math.floor(Math.random() * 5)];
-var ran2 = numbers[Math.floor(Math.random() * 5)];
-var ran3 = numbers[Math.floor(Math.random() * 5)];
-var ran4 = numbers[Math.floor(Math.random() * 5)];
-var ran5 = numbers[Math.floor(Math.random() * 5)];
+var words = [word1, word2, word3, word4, word5];
+var ran1 = words[Math.floor(Math.random() * 5)];
+var ran2 = words[Math.floor(Math.random() * 5)];
+var ran3 = words[Math.floor(Math.random() * 5)];
+var ran4 = words[Math.floor(Math.random() * 5)];
+var ran5 = words[Math.floor(Math.random() * 5)];
 //Thrid, functions.
 function generate() {
-  let num = ran + numbers[Math.floor(Math.random() * 5)];
-  
+  let num = "ran" + numbers[Math.floor(Math.random() * 5)];
+  let ran = "word" + num;
+  let result = "";
+  for (let i = 0; i != input.value; i++) { result += ran }
+  text.innerHTML = result;
+}
+gen.onclick = function() {generate()};
