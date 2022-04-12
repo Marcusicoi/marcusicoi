feeab9a06a89ed89b31e80f3f499439bf7c5c97e
Glitch (marcusicoi) <none>
2022-04-12 05:30:26 +0000

    🎽🌜 Checkpoint ./letter/script.js:27704817/137

--- letter/script.js
+++ letter/script.js
@@ -2,11 +2,11 @@
 const text = document.getElementById("text");
 const gen = document.getElementById("button");
 const input = document.getElementById("input");
-const word1 = document.getElementById("firstword");
-const word2 = document.getElementById("secondword");
-const word3 = document.getElementById("thirdword");
-const word4 = document.getElementById("fourthword");
-const word5 = document.getElementById("fifthword");
+const word1 = document.getElementById("firstword").value;
+const word2 = document.getElementById("secondword").value;
+const word3 = document.getElementById("thirdword").value;
+const word4 = document.getElementById("fourthword").value;
+const word5 = document.getElementById("fifthword").value;
 //Second, define the variables.
 var words = [word1, word2, word3, word4, word5];
 var ran1 = words[Math.floor(Math.random() * words.length)];
@@ -16,9 +16,8 @@ var ran4 = words[Math.floor(Math.random() * words.length)];
 var ran5 = words[Math.floor(Math.random() * words.length)];
 //Thrid, functions.
 function generate() {
-  let o = [ran1, ran2, ran3, ran4, ran5];
-  let ran = o[Math.floor(Math.random() * o.length)];
-  let result = "";
+  let ran = ran1;
+  let result = ran1;
   for (let i = 0; i != input.value; i++) { result += ran }
   text.innerHTML = result;
 }
