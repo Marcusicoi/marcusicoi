5d038aed554d7e79a555149bf505ab004ffef040
Glitch (marcusicoi) <none>
2022-10-24 06:07:40 +0000

    🏍😜 Checkpoint ./florrcr/script.js:27704817/185 ./florrcr/index.html:27704817/27

--- florrcr/script.js
+++ florrcr/script.js
@@ -1,21 +1,21 @@
 //Add variables.
-const random = Math.floor[(Math.Random() * 100];
+const random = Math.floor(Math.random() * 100);
 //C60R?1 TRNG
 function C60SC1() {
   let C60RS1 = document.getElementById("C60RS1");
   let C60AT1 = document.getElementById("C60AT1");
   let C60CH1 = document.getElementById("C60CH1");
   let C60ATT1 = 1;
-  if (60 > random) {
-      C60RS1.innerHTML("Success!");
-      C60AT1.innerHTML("Attempt " + C60ATT1);
-      C60CH1.innerHTML("60% " + "> " + random + "%")
+  if (59 > random) {
+      C60RS1.innerHTML = "Success!";
+      C60AT1.innerHTML = "Attempt " + C60ATT1;
+      C60CH1.innerHTML = "60% " + "> " + (random + 1) + "%";
       C60ATT1 = 1;
   } 
-  if (60 < random) {
-      C60RS1.innerHTML("Failed.");
-      C60AT1.innerHTML("Attempt " + C60ATT1);
-      C60CH1.innerHTML("60% " + "> " + random + "%")
+  if (59 < random) {
+      C60RS1.innerHTML = "Failed.";
+      C60AT1.innerHTML = "Attempt " + C60ATT1;
+      C60CH1.innerHTML = "60% " + "> " + (random + 1) + "%";
       C60ATT1 = (C60ATT1 + 1);
   }; 
 }
