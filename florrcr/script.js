740ff0dd674f005b3c98bf90099254656974541e
Glitch (marcusicoi) <none>
2022-10-24 05:20:11 +0000

    👞🍃 Checkpoint ./florrcr/script.js:27704817/457 ./florrcr/index.html:27704817/42

--- florrcr/script.js
+++ florrcr/script.js
@@ -1,2 +1,12 @@
 //Add variables.
-var chance = ['60', '40', '20', '10', '3']
+var random = Math.floor[Math.random() * 100]
+var attempt = 0
+var tchance = ['60', '40', '20', '10', '3']
+var pchance = ['42.26', '20.15', '5.57', '1.47', '0.14']
+/*ID Variables
+const C60RN1 = document.elementByID("C60RN1");
+const C60RS1 = document.elementByID("C60RS1");*/
+//C60R?1
+C60RN1.onclick = (
+  const C60R) => {
+  
