d493bb354ee194b021518188bde5cc0347cd355b
Glitch (marcusicoi) <none>
2022-11-15 04:19:02 +0000

    💎⏲ Checkpoint ./equation/script.js:27704817/1056

--- equation/script.js
+++ equation/script.js
@@ -20,35 +20,30 @@ document.getElementById("button").onclick = function() {
      '||=', '!', '!=', '!==', '^', 
      '~', '??'
    ];
-   let complicated = num.value / Math.floor(Math.random() * (Math.floor(num.value / 2)));
-   let forLetNum = 0;
    function pOpFunc(type) { return `${type}(${choose(text.value)})` };
-   let pOp = [pOpFunc("sin"), pOpFunc("cos"), pOpFunc("log"), pOpFunc("root")];
+   let pOp = [pOpFunc("sin"), pOpFunc("cos"), pOpFunc("log"), pOpFunc("tan"), pOpFunc("root")];
    let xnum = ['-1', '1', '2', '3', '4', '5'];
    let equation = [];
-   let pOp2 = [];
    for (let i = 0; i < num.value; i++) {
      equation.push(`${choose(op)} ${choose(text.value)}`);
      //Parenthysis.
      if (probability(25)) { 
        equation.push(`${choose(op)} ${choose(pOp)}`) 
      };
-     if (probability(25)) {
-       pOp2.push(`dddddd${choose(pOp)}${choose(text.value)}`)
-       for (let i2 = 0; i < complicated; i2++) {
-          i2 = forLetNum;
-          pOp2.push(`hhh${choose(op)} ${choose(text.value)}`)
-       };
-       if (forLetNum === complicated) { pOp2.push(')') };
-       equation.push(pOp2.join(" ") + " " + choose(op));
-     };  
      //xNUM.
      if (probability(25)) { 
        equation.push(`${choose(op)} (${choose(text.value)})${choose(xnum)}`) 
      };
+     //Fraction.
      if (probability(25)) {
        equation.push(`${choose(op)} frac(${choose(text.value)}/${choose(text.value)})`);
      };
+     //Decimals.
+     if (probability(25)) {
+       equation.push(`${choose(op)} ${choose(text.value)}.${Math.floor(Math.random() * 9) + 1}`);
+       if (probability(10)) {
+         equation.push(`${choose(op)} ${choose(text.value)}.${choose(text.value)}`);
+       };}
    };
    let first = choose(text.value);
    result.innerHTML = first + " " + equation.join(" ");
