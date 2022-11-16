d0e319d5c1006d6659034097ea7af8d2db1356af
Glitch (marcusicoi) <none>
2022-11-16 13:40:18 +0000

    🏝👨 Checkpoint ./name/script.js:27704817/210

--- name/script.js
+++ name/script.js
@@ -9,64 +9,64 @@ submit.onclick = function() {
   let name = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   let names = [];
   for (let i = 0; i < amount.value; i++) {
-  if (NAME === "A") { 
+  if (choose(NAME) === "A") { 
     name = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "u", "r", "s", "t", "v", "w", "x", "y", "z"];
   };
-  if (NAME === "B") {
+  if (choose(NAME) === "B") {
     name = ["a", "e", "h", "i", "l", "o", "r", "u", "w", "y"];
   };
-  if (NAME === "C") {
+  if (choose(NAME) === "C") {
     name = ["a", "e", "h", "i", "l", "o", "r", "u", "w", "y"];
   };
-  if (NAME === "D") {
+  if (choose(NAME) === "D") {
     name = ["a", "e", "h", "i", "l", "o", "r", "u", "w", "y"];
   };
-  if (NAME === "E") {
+  if (choose(NAME) === "E") {
     name = ["a", "b", "c", "d", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   };
-  if (NAME === "F") {
+  if (choose(NAME) === "F") {
     name = ["a", "e", "h", "i", "l", "o", "r", "u", "w", "y"];
   };
-  if (NAME === "G") {
+  if (choose(NAME) === "G") {
     name = ["a", "e", "h", "i", "l", "o", "r", "u", "y"];
   };
-  if (NAME === "H") {
+  if (choose(NAME) === "H") {
     name = ["a", "e", "i", "o", "u", "w", "y"];
   };
-  if (NAME === "I") {
+  if (choose(NAME) === "I") {
     name = ["a", "b", "c", "d", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   };
-  if (NAME === "J") {
+  if (choose(NAME) === "J") {
     name = ["a", "e", "h", "i", "o", "r", "u", "w", "y"];
   };
-  if (NAME === "K") {
+  if (choose(NAME) === "K") {
     name = ["a", "e", "h", "i", "l", "o", "r", "s", "u", "w", "y"];
   };
-  if (NAME === "L") {
+  if (choose(NAME) === "L") {
     name = ["a", "e", "h", "i", "l", "o", "r", "u", "y"];
   };
-  if (NAME === "M") {
+  if (choose(NAME) === "M") {
     name = ["a", "e", "i", "o", "r", "u", "w", "y"];
   };
-  if (NAME === "N") {
+  if (choose(NAME) === "N") {
     name = ["a", "e", "i", "o", "u", "w", "y"];
   };
-  if (NAME === "O") {
+  if (choose(NAME) === "O") {
     name = ["a", "b", "c", "d", "f", "g", "h", "i", "j", "k", "l", "m", "n", "p", "o", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   };
-  if (NAME === "P") {
+  if (choose(NAME) === "P") {
     name = ["a", "e", "h", "i", "l", "o", "r", "s", "t", "u", "w", "y"];
   };
-  if (NAME === "Q") {
+  if (choose(NAME) === "Q") {
     name = ["a", "e", "i", "l", "r", "s", "u", "w"];
   };
-  if (NAME === "R") {
+  if (choose(NAME) === "R") {
     name = ["a", "e", "h", "i", "j", "o", "r", "s", "t", "u", "w", "y"];
   };
-  if (NAME === "S") {
+  if (choose(NAME) === "S") {
     name = ["a", "c", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "t", "u", "w", "y", "z"];
   };
-  if (NAME === "T") {
+  if (choose(NAME) === "T") {
     name = ["a", "e", "f", "h", "i", "k", "l", "o", "r", "u", "w", "y", "z"];
   };
   names.push(choose(name));
