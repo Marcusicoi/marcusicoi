eaf6624e3fa0e82525ce1b575a5a4e251cbffff5
Glitch (marcusicoi) <none>
2022-11-16 09:28:02 +0000

    👸🔮 Checkpoint ./name/index.html:27704817/1887 ./name/script.js:27704817/362

---
+++ name/script.js
@@ -0,0 +1,10 @@
+let amount = document.getElementById("amount");
+let submit = document.getElementById("submit");
+let result = document.getElementById("result");
+function choose(argument) {
+  return argument[Math.floor(Math.random() * argument.length)]
+};
+let name = [
+submit.onclick = function() {
+  
+};
