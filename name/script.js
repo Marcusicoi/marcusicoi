637e28fe4baed5fa30e156a0d118303a8a070695
Glitch (marcusicoi) <none>
2022-11-16 09:40:00 +0000

    🏍🙅 Checkpoint ./name/script.js:27704817/770

--- name/script.js
+++ name/script.js
@@ -4,7 +4,18 @@ let result = document.getElementById("result");
 function choose(argument) {
   return argument[Math.floor(Math.random() * argument.length)]
 };
-let name = [
+let NAME = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+let name = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+let names = [];
 submit.onclick = function() {
-  
+  let RAN = choose(NAME);
+  let ran = choose(name);
+  for (let i = 0; i < amount.value; i++) {
+  if (NAME === "A") { 
+    name = ["b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "u", "r", "s", "t", "v", "w", "x", "y", "z"];
+  };
+  if (NAME === "B") {
+    name = ["a", "cdehiloruwy
+  }
+  };
 };
