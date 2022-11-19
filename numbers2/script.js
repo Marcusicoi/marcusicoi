82a5e72c2890885615c6d816e65b500c1348351b
Glitch (marcusicoi) <none>
2022-11-19 05:58:28 +0000

    🚁🦄 Checkpoint ./numbers2/script.js:27704817/1044 ./numbers2/index.html:27704817/212

--- numbers2/script.js
+++ numbers2/script.js
@@ -1,11 +1,26 @@
 let digit = 4;
 //Table 1
 let Numbers1Array = ["Numbers", "Thousand", "Million", "Billion", "Trillion", "Quadrillion", "Quintillion", "Sextillion", "Septillion", "Octillion", "Nonillion"];
-let Units1Array = ["", "Un-", "Duo", "Tre-, Tres-", "Quattor-", "Quin-", "Sex, Ses-", "Octo-", "Novem-, Noven-"];
-let Abbs1Array = ["K", "U, M", "D, B", "T", "q", "Q", "s", "S", "O", "N"];
+let Units1Array = ["", "", "Un-", "Duo", "Tre-, Tres-", "Quattor-", "Quin-", "Sex, Ses-", "Sept-", "Octo-", "Novem-, Noven-"];
+let Abbs1Array = ["", "K", "U, M", "D, B", "T", "q", "Q", "s", "S", "O", "N"];
 let Row1 = [];
 let ID1 = document.getElementById("Level1Table");
-ID1.innerHTML = "
 for (let i = 0; i < 11; i++) {
-  
-}
+  Row1.push(`
+  <tr>
+    <th>${Numbers1Array[i]}</th>
+    <th>${Units1Array[i]}</th>
+    <th>${digit}</th>
+    <th>${Abbs1Array[i]}</th>
+  </tr>
+  `);
+};
+ID1.innerHTML = `
+  <table>
+  <tr>
+    <th>Numbers</th>
+    <th>Units</th>
+    <th>Digits</th>
+    <th>Abbreviated</th>
+  </tr>
+` + Row1.join(" ");
