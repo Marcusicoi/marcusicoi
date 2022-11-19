4f14b0eab7cd851cd20ccad695e97582bde64e4f
Glitch (marcusicoi) <none>
2022-11-19 05:00:11 +0000

    🏡🏪 Checkpoint ./numbers2/script.js:27704817/315

--- numbers2/script.js
+++ numbers2/script.js
@@ -1,19 +1,23 @@
 function Level1Table(length, width) {
-  var L1Table = document.getElementById("Level1Table");
-  var table = document.createElement('TABLE');
+  let L1Table = document.getElementById("Level1Table");
+  let NumbersArray = [
+    "Numbers", 
+    "Thousand", "Million", "Billion", "Trillion", "Quadrillion", "Quintillion", 
+    "Sextillion", "Septillion", "Octillion", "Nonillion"
+  ];
+  let table = document.createElement('TABLE');
   table.border = '1';
-  var tableBody = document.createElement('TBODY');
+  let tableBody = document.createElement('TBODY');
   table.appendChild(tableBody);
-  for (var x = 0; x < length; x++) {
-    var tr = document.createElement('TR');
+  for (let x = 0; x < length; x++) {
+    let tr = document.createElement('TR');
     tableBody.appendChild(tr);
-    for (var y = 0; y < width; y++) {
-      var td = document.createElement('TD');
-      td.width = '75';
-      td.appendChild(document.createTextNode("Cell " + x + "," + y));
+    for (let y = 0; y < width; y++) {
+      let td = document.createElement('TD');
+      td.appendChild(document.createTextNode(NumbersArray[x] + "," + y));
       tr.appendChild(td);
     }
   }
   L1Table.appendChild(table);
 }
-Level1Table(10, 4);
+Level1Table(11, 4);
