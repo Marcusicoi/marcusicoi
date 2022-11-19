9fbcabeae033615e1d8ade9e22d073ff4aedb4ea
Glitch (marcusicoi) <none>
2022-11-19 05:46:23 +0000

    🙈💅 Checkpoint ./numbers2/script.js:27704817/3416

--- numbers2/script.js
+++ numbers2/script.js
@@ -3,39 +3,9 @@ let digit = 4;
 let Numbers1Array = ["Numbers", "Thousand", "Million", "Billion", "Trillion", "Quadrillion", "Quintillion", "Sextillion", "Septillion", "Octillion", "Nonillion"];
 let Units1Array = ["", "Un-", "Duo", "Tre-, Tres-", "Quattor-", "Quin-", "Sex, Ses-", "Octo-", "Novem-, Noven-"];
 let Abbs1Array = ["K", "U, M", "D, B", "T", "q", "Q", "s", "S", "O", "N"];
-//The function
-function createTable(array, type, title) {
-  let body = document.getElementById('Level1Table');
-  let tbl = document.createElement('table');
-  tbl.border = 1;
-  let thead = document.createElement('thead');
-  let thr = document.createElement('tr');
-  title.forEach((title) => {
-    let th = document.createElement('th');
-    th.appendChild(document.createTextNode(title));
-    thr.appendChild(th);
-  });
-  thead.appendChild(thr);
-  tbl.appendChild(thead);
-  let tbdy = document.createElement('tbody');
-  let tr = document.createElement('tr');
-  array.forEach((object) => {
-    let tr = document.createElement('tr');
-    type.forEach((type) => {
-      var td = document.createElement('td');
-      td.appendChild(document.createTextNode(object[type]));
-      tr.appendChild(td);
-    });
-    tbdy.appendChild(tr);    
-  });
-  tbl.appendChild(tbdy);
-  body.appendChild(tbl)
-  return tbl;
-};
-let row = [];
+let Row1 = [];
+let ID1 = document.getElementById("Level1Table");
+ID1.innerHTML = "
 for (let i = 0; i < 11; i++) {
-  let N = Numbers1Array[i], U = Units1Array[i], A = Abbs1Array[i];
-  digit += (3 * i)
-  row.push({number: N, unit: U, digits: digit, abbs: A})
-};
-createTable([{number:], ['number', 'unit', 'digits', 'abbs'], ['Numbers', 'Units', 'Digits', 'Abbreviations'])
+  
+}
