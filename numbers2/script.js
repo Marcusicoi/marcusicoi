8aa76301ba14083c8b83862120e8684d87ca2027
Glitch (marcusicoi) <none>
2022-11-19 06:34:44 +0000

    📺📒 Checkpoint ./numbers2/script.js:27704817/1325

--- numbers2/script.js
+++ numbers2/script.js
@@ -28,18 +28,19 @@ ID1.innerHTML = `
 ` + Row1.join(" ");
 //Table 2
 let Numbers2Array = ["Decillion", "Vigintillion", "Trigintillion", "Quadragintillion", "Quinquagintillion", "Sexagintillion", "Septuagintillion", "Octagintillion", "Nonagintillion"];
-let Units2Array = ["", "Un-", "Duo", "Tre-, Tres-", "Quattor-", "Quin-", "Sex, Ses-", "Sept-", "Octo-", "Novem-, Noven-"];
-let Abbs2Array = ["K", "U, M", "D, B", "T", "q", "Q", "s", "S", "O", "N"];
+let Units2Array = ["Deci-", "Viginti-", "Triginta-", "Quadraginta-", "Quinquaginta-", "Sexaginta-", "Septuaginta-", "Octaginta-", "Nonaginta-"];
+let Digits2Array = [digit + 3, digit + (3 * 2)]
+let Abbs2Array = ["D", "V", "TR", "QU", "QI", "Sx", "Sp", "Oc", "No"];
 let Row2 = [];
 let ID2 = document.getElementById("Level2Table");
-for (let i = 0; i < 10; i++) {
+for (let i = 0; i < 9; i++) {
   let op = 0;
-  do { op += 1; digit === 34; digit += (3 * op) } while (op === 21);
+  do { op += 10; digit + 3; digit += (3 * op) } while (op === 21);
   Row2.push(`
   <tr>
     <th>${Numbers2Array[i]}</th>
     <th>${Units2Array[i]}</th>
-    <th>${digit}</th>
+    <th>${Digits2Array[i]}</th>
     <th>${Abbs2Array[i]}</th>
   </tr>
   `);
@@ -53,3 +54,28 @@ ID2.innerHTML = `
     <th>Abbreviated</th>
   </tr>
 ` + Row2.join(" ");
+//Table 3
+let Numbers3Array = ["Decillion", "Vigintillion", "Trigintillion", "Quadragintillion", "Quinquagintillion", "Sexagintillion", "Septuagintillion", "Octagintillion", "Nonagintillion"];
+let Units3Array = ["Deci-", "Viginti-", "Triginta-", "Quadraginta-", "Quinquaginta-", "Sexaginta-", "Septuaginta-", "Octaginta-", "Nonaginta-"];
+let Abbs3Array = ["D", "V", "TR", "QU", "QI", "Sx", "Sp", "Oc", "No"];
+let Row3 = [];
+let ID3 = document.getElementById("Level3Table");
+for (let i = 0; i < 9; i++) {
+  Row2.push(`
+  <tr>
+    <th>${Numbers3Array[i]}</th>
+    <th>${Units3Array[i]}</th>
+    <th>${digit}</th>
+    <th>${Abbs3Array[i]}</th>
+  </tr>
+  `);
+};
+ID3.innerHTML = `
+  <table>
+  <tr>
+    <th>Numbers</th>
+    <th>Units</th>
+    <th>Digits</th>
+    <th>Abbreviated</th>
+  </tr>
+` + Row3.join(" ");
