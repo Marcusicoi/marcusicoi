bc99814288d4b9741d035aed2f78dd1e7005c2b1
Glitch (marcusicoi) <none>
2022-11-19 10:10:49 +0000

    📡🛬 Checkpoint ./numbers/script.js:27704817/1977

--- numbers/script.js
+++ numbers/script.js
@@ -13,6 +13,16 @@ function headcode() {
     <th ${headcolor()}>Abbreviated</th>
   </tr>
   `;           
+};
+function code(color, number, unit, digit, abb) {
+  return `
+  <tr>
+    <th ${colors(color)}>${number}</th>
+    <th ${colors(color)}>${unit}</th>
+    <th ${colors(color)}>10^${digit}</th>
+    <th ${colors(color)}>${abb}</th>
+  </tr>
+  `
 }
 //Table 1
 let Numbers1Array = ["Thousand", "Million", "Billion", "Trillion", "Quadrillion", "Quintillion", "Sextillion", "Septillion", "Octillion", "Nonillion"];
@@ -27,14 +37,7 @@ for (let i = 0; i < 10; i++) {
   let op2 = 0;
   do { op += 1; Digit1 += (3 * op) } while (op === 0);
   do { op2 += 1; CLnum1 += op2 } while (op2 === 0);
-  Row1.push(`
-  <tr>
-    <th ${colors(CLnum1)}>${Numbers1Array[i]}</th>
-    <th ${colors(CLnum1)}>${Units1Array[i]}</th>
-    <th ${colors(CLnum1)}>10^${Digit1}</th>
-    <th ${colors(CLnum1)}>${Abbs1Array[i]}</th>
-  </tr>
-  `);
+  Row1.push(code(CLnum1, Numbers1Array[i], Units1Array[i], Digit1, Abbs1Array[i]));
 };
 ID1.innerHTML = headcode() + Row1.join(" ");
 //Table 2
@@ -42,77 +45,46 @@ let Numbers2Array = ["Decillion", "Vigintillion", "Trigintillion", "Quadragintil
 let Units2Array = ["Deci-", "Viginti-", "Triginta-", "Quadraginta-", "Quinquaginta-", "Sexaginta-", "Septuaginta-", "Octaginta-", "Nonaginta-"];
 let Digit2 = 3;
 let Abbs2Array = ["D", "V", "TR", "QU", "QI", "Sx", "Sp", "Oc", "No"];
-let BGnum2 = -1;
+let CLnum2 = -1;
 let Row2 = [];
 let ID2 = document.getElementById("Level2Table");
 for (let i = 0; i < 9; i++) {
   let op = 0;
   let op2 = 0;
   do { op += 10; Digit2 + 3; Digit2 += (3 * op) } while (op === 0);
-  do { op2 += 1; BGnum2 += op2 } while (op === 0);
-  Row2.push(`
-  <tr>
-    <th>${Numbers2Array[i]}</th>
-    <th>${Units2Array[i]}</th>
-    <th>10^${Digit2}</th>
-    <th>${Abbs2Array[i]}</th>
-  </tr>
-  `);
+  do { op2 += 1; CLnum2 += op2 } while (op === 0);
+  Row2.push(code(CLnum2, Numbers2Array[i], Units2Array[i], Digit2, Abbs2Array[i]));
 };
-ID2.innerHTML = headCode() + Row2.join(" ");
+ID2.innerHTML = headcode() + Row2.join(" ");
 //Table 3
 let Numbers3Array = ["Centillion", "Ducentillion", "Trecentillion", "Quadringentillion", "Quingentillion", "Sescentillion", "Septingentillion", "Octingentillion", "Nonagentillion"];
 let Units3Array = ["Centi-", "Ducenti-", "Trecenti-", "Quadringenti-", "Quingenti-", "Sescenti-", "Septingenti-", "Octingenti-", "Nonagenti-"];
 let Digit3 = 3;
 let Abbs3Array = ["Ce", "Du", "Te", "Qd", "Qn", "Sc", "St", "Ot", "Ng"];
+let CLnum3 = -1;
 let Row3 = [];
 let ID3 = document.getElementById("Level3Table");
 for (let i = 0; i < 9; i++) {
   let op = 0;
+  let op2 = 0;
   do { op += 100; Digit3 + 3; Digit3 += (3 * op) } while (op === 0);
-  Row3.push(`
-  <tr>
-    <th>${Numbers3Array[i]}</th>
-    <th>${Units3Array[i]}</th>
-    <th>10^${Digit3}</th>
-    <th>${Abbs3Array[i]}</th>
-  </tr>
-  `);
+  do { op2 += 1; CLnum3 += op2 } while (op === 0);
+  Row3.push(code(CLnum3, Numbers3Array[i], Units3Array[i], Digit3, Abbs3Array[i]));
 };
-ID3.innerHTML = `
-  <table>
-  <tr>
-    <th ${headcolor()}>Numbers</th>
-    <th ${headcolor()}>Units</th>
-    <th ${headcolor()}>Digits</th>
-    <th ${headcolor()}>Abbreviated</th>
-  </tr>
-` + Row3.join(" ");
+ID3.innerHTML = headcode() + Row3.join(" ");
 //Table 4
 let Numbers4Array = ["Millillion", "Dumillillion", "Trimillillion", "Quadrimillilliion", "Quinmillillion", "Sexmillillion", "Septimillillion", "Octimillillion", "Nonimillillion"];
 let Units4Array = ["Milli-", "Dumilli-", "Trimilli-", "Quadrimilli-", "Quinmilli-", "Sexmilli-", "Septimilli-", "Octimilli-", "Nonimilli-"];
 let Digit4 = 3;
 let Abbs4Array = ["ML", "DL", "TL", "qL", "QL", "sL", "SL", "OL", "NL"];
+let CLnum4 = -1;
 let Row4 = [];
 let ID4 = document.getElementById("Level4Table");
 for (let i = 0; i < 9; i++) {
   let op = 0;
+  let op2 = 0;
   do { op += 1000; Digit4 + 3; Digit4 += (3 * op) } while (op === 0);
-  Row4.push(`
-  <tr>
-    <th>${Numbers4Array[i]}</th>
-    <th>${Units4Array[i]}</th>
-    <th>10^${Digit4}</th>
-    <th>${Abbs4Array[i]}</th>
-  </tr>
-  `);
+  do { op2 += 1; CLnum4 += op2 } while (op === 0);
+  Row4.push(code(CLnum4, Numbers4Array[i], Units4Array[i], Digit4, Abbs4Array[i]));
 };
-ID4.innerHTML = `
-  <table>
-  <tr>
-    <th ${headcolor()}>Numbers</th>
-    <th ${headcolor()}>Units</th>
-    <th ${headcolor()}>Digits</th>
-    <th ${headcolor()}>Abbreviated</th>
-  </tr>
-` + Row4.join(" ");
+ID4.innerHTML = headcode() + Row4.join(" ");
