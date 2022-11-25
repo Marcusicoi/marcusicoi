fe1cb2bb17f296c047a4bd3244f81867eb99fc6b
Glitch (marcusicoi) <none>
2022-11-25 12:32:38 +0000

    🚵⏰ Checkpoint ./florrcr/index.html:27704817/1578 ./florrcr/script.js:27704817/1037

--- florrcr/script.js
+++ florrcr/script.js
@@ -6,7 +6,7 @@ function probability(n) {
 function oProbability(n) {
   return Math.random() > (n / 100);
 }
-//C60R?1 TRNG
+//C60??1 TRNG
 document.getElementById("C60BT1").onclick = function() {
   let C60RS1 = document.getElementById("C60RS1");
   let C60CH1 = document.getElementById("C60CH1");
@@ -17,7 +17,31 @@ document.getElementById("C60BT1").onclick = function() {
       C60RS1.innerHTML = "Failed.";
       C60CH1.innerHTML = "60% " + "> " + (Math.floor(Math.random() * 40) + 60) + "%";
   }; 
-}
+};
+//U40??1 TRNG
+document.getElementById("U40BT1").onclick = function() {
+  let U40RS1 = document.getElementById("U40RS1");
+  let U40CH1 = document.getElementById("U40CH1");
+  if (probability(40)) {
+      U40RS1.innerHTML = "Success!";
+      U40CH1.innerHTML = "40% " + "> " + (Math.floor(Math.random() * 40)) + "%";
+  } else {
+      U40RS1.innerHTML = "Failed.";
+      U40CH1.innerHTML = "40% " + "> " + (Math.floor(Math.random() * 60) + 40) + "%";
+  }; 
+};
+//R20??1 TRNG
+document.getElementById("R20BT1").onclick = function() {
+  let R20RS1 = document.getElementById("R20RS1");
+  let R20CH1 = document.getElementById("R20CH1");
+  if (probability(20)) {
+      R20RS1.innerHTML = "Success!";
+      R20CH1.innerHTML = "20% " + "> " + (Math.floor(Math.random() * 20)) + "%";
+  } else {
+      R20RS1.innerHTML = "Failed.";
+      R20CH1.innerHTML = "20% " + "> " + (Math.floor(Math.random() * 80) + 20) + "%";
+  }; 
+};
 //Many Petals
 document.getElementById("C60BT2").onclick = function() {
   let C60IP2 = document.getElementById("C60IP2");
