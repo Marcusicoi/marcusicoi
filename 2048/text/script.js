d14c0887cc10bb2a136c5f92e0763e4a1213b838
Glitch (marcusicoi) <none>
2022-12-11 07:12:33 +0000

    🐜🐾 Checkpoint ./2048/text/script.js:27704817/1068

--- 2048/text/script.js
+++ 2048/text/script.js
@@ -5,14 +5,28 @@ let level = document.getElementById("level");
 //The counters
 var levelc = 1;
 var num = 2n;
-var array = ["K", "M", "B"]
-var arrayN = 0;
+function abbreviateNumber(value) {
+    var newValue = value;
+    if (value >= 1000) {
+        var suffixes = ["", "K", "M", "B","T"];
+        var suffixNum = Math.floor( (""+value).length/3 );
+        var shortValue = '';
+        for (var precision = 2; precision >= 1; precision--) {
+            shortValue = parseFloat( (suffixNum != 0 ? (value / Math.pow(1000,suffixNum) ) : value).toPrecision(precision));
+            var dotLessShortValue = (shortValue + '').replace(/[^a-zA-Z 0-9]+/g,'');
+            if (dotLessShortValue.length <= 2) { break; }
+        }
+        if (shortValue % 1 != 0)  shortValue = shortValue.toFixed(1);
+        newValue = shortValue+suffixes[suffixNum];
+    }
+    return newValue;
+}
 //If you click the button
 inc.onclick = function() {
   num *= 2n;
   levelc += 1;
-  number.innerHTML = num
+  number.innerHTML = abbreviateNumber(num)
   level.innerHTML = "Level " + levelc;
   //If statements
-  if (num.length === 4) {
-};
+ // if (num.length >= 4) { number.innerHTML = abbreviateNumber(num)};
+}
