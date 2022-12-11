60074066cf78baa892dacef51ab0eabcef7f47c2
Glitch (marcusicoi) <none>
2022-12-11 07:01:30 +0000

    💒💎 Checkpoint ./2048/text/script.js:27704817/535

--- 2048/text/script.js
+++ 2048/text/script.js
@@ -4,11 +4,15 @@ let inc = document.getElementById("increaser");
 let level = document.getElementById("level");
 //The counters
 var levelc = 1;
-var num = 2;
+var num = 2n;
+var array = ["K", "M", "B"]
+var arrayN = 0;
 //If you click the button
 inc.onclick = function() {
-  num *= 2;
+  num *= 2n;
   levelc += 1;
-  number.innerHTML = num.toPrecision(1);
+  number.innerHTML = num
   level.innerHTML = "Level " + levelc;
-}
+  //If statements
+  if (num.length === 4) {
+};
