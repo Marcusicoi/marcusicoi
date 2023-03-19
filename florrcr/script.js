f89c3455709f120b8b7edeca6990e9e0fc9ed404
Glitch (marcusicoi) <none>
2023-03-19 13:03:46 +0000

    🙉🤑 Checkpoint ./florrcr/script.js:27704817/315

--- florrcr/script.js
+++ florrcr/script.js
@@ -86,6 +86,8 @@ let uCombo = 0;
 let uIsPRNG = false;
 let uProbArray = [42.26, 84.53, 100];
 let uHSattC = 1;
+localStorage.setItem("uHSattCnt", uHSattC);
+let uHSattC2 = localStorage.getItem("u
 uHSatt.innerHTML = "Highest Attempt: " + localStorage.getItem("uHSattCnt");
 uOriginal.onclick = function() {
   uProb = 75;
@@ -167,9 +169,8 @@ selectId("uButton").onclick = function() {
       result.innerHTML = "Failed.";
       chance.innerHTML = uIsPRNG === true ? `${uProb}% > ${(((Math.random() * ((100 - uProb) + 1)) + uProb).toFixed(2))}%` : `${uProb}% > ${(Math.floor(Math.random() * ((100 - uProb) + 1)) + uProb)}%`;
       uAtt = uAtt + 1;
-      uHSattC = uAtt;
-      localStorage.setItem("uHSattCnt", uHSattC);
-      if (uHSattC > localStorage.getItem("uHSattCnt")) {
+      if (uAtt > uHSattC) { uHSattC = uAtt };
+      if (uHSAttC > localStorage.getItem("uHSattCnt")) {
           uHSatt.innerHTML = "Highest Attempt: " + (localStorage.getItem("uHSattCnt"));
       };
       if (uIsPRNG === true) { 
