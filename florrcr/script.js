872579ab438f1ca80cf0c0e4f960ffaf8e217ad8
Glitch (marcusicoi) <none>
2023-03-19 12:19:49 +0000

    😚🏛 Checkpoint ./florrcr/index.html:27704817/133 ./florrcr/script.js:27704817/385

--- florrcr/script.js
+++ florrcr/script.js
@@ -77,12 +77,15 @@ let uOldMSP = selectId("uTypeOldMSkill");
 let uCurrent = selectId("uTypeCurrent");
 let uLSP = selectId("uTypeLSkill");
 let uMSP = selectId("uTypeMSkill");
+
+let uHSatt = selectId("uHSAttempt");
 //Stuff;
 let uProb = 40;
 let uAtt = 0;
 let uCombo = 0;
 let uIsPRNG = false;
 let uProbArray = [42.26, 84.53, 100];
+let uHSattC = 1;
 uOriginal.onclick = function() {
   uProb = 75;
   uIsPRNG = false;
@@ -163,6 +166,12 @@ selectId("uButton").onclick = function() {
       result.innerHTML = "Failed.";
       chance.innerHTML = uIsPRNG === true ? `${uProb}% > ${(((Math.random() * ((100 - uProb) + 1)) + uProb).toFixed(2))}%` : `${uProb}% > ${(Math.floor(Math.random() * ((100 - uProb) + 1)) + uProb)}%`;
       uAtt = uAtt + 1;
+      if (uAtt > uHSattC) {
+          us
+          if (localStorage.uHSattCnt) {
+              localStorage
+          }
+      }
       if (uIsPRNG === true) { 
         uProb = uProbArray[uAtt];
         selectId("uSuccess").innerHTML = uProb + "% Success Chance";
