f1f17b358da3565a5512852f18a0ace0b18a0aaf
Glitch (marcusicoi) <none>
2023-04-11 08:14:55 +0000

    👹💒 Checkpoint ./florrcr/script2.js:27704817/800

--- florrcr/script2.js
+++ florrcr/script2.js
@@ -14,9 +14,13 @@ function MULS(amount) {
   let loseThree = 0
 }
 
-function untilSucceed(chance, mob) {
+function untilSucceed(chance, mob, petal) {
   let attempts = 1
   let succeeded = 0
+  let average = (1 / chance)
+  let overageL = `Your luckiness is higher than the average (${average})!`
+  let underageL = `Your luckiness is lower than the average (${average}).`
+  let ouageL = average > attempts ? overageL : underageL
   do  {
     if (ran(chance) > Math.random()) {
         succeeded += 1
@@ -25,5 +29,9 @@ function untilSucceed(chance, mob) {
     }
   } while (succeeded === 0)
   console.log(`
-  You need
+  The chance of ${drop} from ${mo
+  You need ${average} attempts for it to drop ${petal} from ${mob}!
+  But you got ${attempts} attempts! ${ouageL}
+  `)
 }
+function untilSucceed(0.03,
