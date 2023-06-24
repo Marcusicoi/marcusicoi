7582f7cde298533540ddb55f13b87e64849246a9
Glitch (marcusicoi) <none>
2023-06-24 04:03:23 +0000

    📅🍨 Checkpoint ./florr/rarities/petal/script.js:27704817/520

--- florr/rarities/petal/script.js
+++ florr/rarities/petal/script.js
@@ -4,7 +4,29 @@ let inpRarity = document.getElementById("inpRarity")
 let button = document.getElementById("button")
 let result = document.getElementById("result")
 
-let hltBase = {
-  basic: 10/3
+let cdBase = {
+  basic: 2.5
 }
 
+let hltBase = {
+  basic: 10/(3^3)
+}
+
+let dmgBase = {
+  basic: 10/(3^3)
+}
+
+let description = {
+  basic: "A nice petal, not too strong, but not too weak."
+}
+
+let stats = `
+
+`
+button.onclick = function(){
+  if (inpPetal.value === "Basic") {
+    if (inpRarity.value === "Everywhere") {
+      
+    }
+  }
+}
