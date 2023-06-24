2a6241fb69b0e6891572bba77e9fb545b99c1fee
Glitch (marcusicoi) <none>
2023-06-24 03:17:34 +0000

    👧🍔 Checkpoint ./florr/rarities/petal/style.css:27704817/29 ./florr/rarities/petal/index.html:27704817/876 ./florr/rarities/petal/script.js:27704817/190

--- florr/rarities/petal/script.js
+++ florr/rarities/petal/script.js
@@ -3,4 +3,7 @@ let inpRarity = document.getElementById("inpRarity")
 
 let button = document.getElementById("button")
 let result = document.getElementById("result")
+let canvas = document.getElementyBId("canvres")
 
+var ctx = canvas.getContext("2d");
+ctx.strokeText("Basic",10,50)
