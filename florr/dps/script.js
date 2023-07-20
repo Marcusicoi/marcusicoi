fb485390fc07dcf78a5f0784a1f75aa25cf31eb2
Glitch (marcusicoi) <none>
2023-07-20 23:37:51 +0000

    🌔🎁 Checkpoint ./florr/dps/index.html:27704817/650 ./florr/dps/script.js:27704817/1337

---
+++ florr/dps/script.js
@@ -0,0 +1,6 @@
+let faster = document.getElementById("faster")
+let rotation = document.getElementById("rotation")
+let button = document.getElementById("button")
+
+let fastR = ["Common", "Unusual", "Rare", "Epic", "Legendary"]
+let rotatR
