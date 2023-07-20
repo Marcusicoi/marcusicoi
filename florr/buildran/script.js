78198f2ca7a148dc5c06139d4a3035c2116f44cd
Glitch (marcusicoi) <none>
2023-07-20 14:46:04 +0000

    🎾🍓 Checkpoint ./florr/buildran/script.js:27704817/6061 ./florr/buildran/index.html:27704817/176

--- florr/buildran/script.js
+++ florr/buildran/script.js
@@ -1,4 +1,84 @@
 const slots = document.getElementById("slotnum")
 const button = document.getElementById("butran")
+const first = document.getElementById("first")
+const second = document.getElementById("second")
 
-let petals = ["
+let petals = [
+  "Air",
+  "Ankh",
+  "Ant Egg",
+  "Antennae",
+  "Basic",
+  "Basil",
+  "Beetle Egg",
+  "Bone",
+  "Bubble",
+  "Bulb",
+  "Bur",
+  "Cactus",
+  "Carrot",
+  "Claw",
+  "Clover",
+  "Corn",
+  "Cotton",
+  "Cutter",
+  "Dahlia",
+  "Dandelion",
+  "Fangs",
+  "Faster",
+  "Grapes",
+  "Heavy",
+  "Honey",
+  "Iris",
+  "Jelly",
+  "Leaf",
+  "Light",
+  "Lightning",
+  "Lotus",
+  "Magnet",
+  "Mark",
+  "Missile",
+  "Orange",
+  "Pearl",
+  "Peas",
+  "Pincer",
+  "Plank",
+  "Pollen",
+  "Poo",
+  "Powder",
+  "Relic",
+  "Rice",
+  "Rock",
+  "Root",
+  "Rose",
+  "Rubber",
+  "Salt",
+  "Sand",
+  "Shell",
+  "Soil",
+  "Sponge",
+  "Square",
+  "Starfish",
+  "Stick",
+  "Stinger",
+  "Blood Stinger",
+  "Third Eye",
+  "Tomato",
+  "Uranium",
+  "Web",
+  "Wing",
+  "Yggdrasil",
+  "Yin-yang",
+  "Yucca",
+]
+
+function petal() {
+  return petals[Math.floor[Math.random() * petals.length]]
+}
+
+button.onclick = function() {
+if (slots.value === 10) {
+  first.indexHTML = "petal(), petal(), petal()"
+  second.indexHTML = "petal(), petal(), petal()"
+}
+}
