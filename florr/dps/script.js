750527f8face4beb759e7eb2d8d0d18990dd0a61
Glitch (marcusicoi) <none>
2023-07-21 00:01:52 +0000

    🎗🍀 Checkpoint ./florr/dps/script.js:27704817/164

--- florr/dps/script.js
+++ florr/dps/script.js
@@ -1,7 +1,3 @@
 let faster = document.getElementById("faster")
 let rotation = document.getElementById("rotation")
 let button = document.getElementById("button")
-
-let fastR = ["Common", "Unusual", "Rare", "Epic", "Legendary"]
-let rotateR = ["Common", "Unusual", "Rare", "Epic", "Legendary", "Mythic", "Ultra"]
-
