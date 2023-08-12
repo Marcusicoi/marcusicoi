b08a3274eb6f24135251f8a96bd2d9ed527188d0
Glitch (marcusicoi) <none>
2023-08-12 14:43:59 +0000

    🚖📹 Checkpoint ./smashhit/script.js:27704817/497 ./smashhit/index.html:27704817/62

--- smashhit/script.js
+++ smashhit/script.js
@@ -16,23 +16,5 @@ function randomSong() {
 ID("button").onclick = function (){
 ID("button").style.visibility = "hidden"
 ID(randomSong()).play()
-//SECONDS
-var seconds = 0
-
-function incSeconds() { 
-  seconds += 1
-  ID("secdisp").innerText = seconds + " Seconds"
-}
-
-let secInterval = setInterval(incSeconds, 1000)
-  
-if (seconds === 5) {
-  seconds - 500
-  //clearInterval(secInterval)
-}
-  
-if (seconds === 0) {
-  secInterval
-} 
-
+setInterval(function() {ID(randomSong()).play()}, 32000)
 }
