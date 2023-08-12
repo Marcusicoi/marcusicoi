3c91702ad376819426dbf6c24d04e38c76405f6d
Glitch (marcusicoi) <none>
2023-08-12 13:58:28 +0000

    🦀💳 Checkpoint ./smashhit/index.html:27704817/562 ./smashhit/script.js:27704817/618

--- smashhit/script.js
+++ smashhit/script.js
@@ -1,5 +1,19 @@
+// im lazy ;-;
 function ID(id) {
   return document.getElementById(id)
 }
 
-ID("Versus1").play();
+let Songs = [
+"Versus1", "Versus2"
+]
+
+function randomSong() {
+  return Songs[Math.floor(Math.random() * Songs.length)]
+}
+
+
+//PLAY!!
+ID("button").onclick = function (){
+ID("button").style.visibility = "hidden"
+ID(randomSong()).play()
+}
