c84bae00e0b2f3c2fa93ef9c04e1b6407253bda9
Glitch (marcusicoi) <none>
2023-08-13 05:31:54 +0000

    🚕🏮 Checkpoint ./smashhit/script.js:27704817/18 ./smashhit/index.html:27704817/1598

--- smashhit/script.js
+++ smashhit/script.js
@@ -5,21 +5,21 @@ function ID(id) {
 
 //Randomize Songs
 let Songs = [
-"C0S1", "C0S2", "C0S3",
+/*"C0S1", "C0S2", "C0S3",
 "C1S1", "C1S2", "C1S3",
 "C2S1", "C2S2", "C2S3", "C2S4",
 "C3S1", "C3S2", "C3S3", "C3S4",
 "C4S1", "C4S2",
 "C5S1",         "C5S3", "C5S4",
-"C6S1", "C6S2", "C6S3", "C6S4",
-"C7S1", "C7S2", "C7S3",
+"C6S1", "C6S2", "C6S3", "C6S4",*/
+"C7S1", "C7S2", "C7S3",/*
                 "C8S3",
 "C9S1", "C9S2", "C9S3", "C9S3P2",
 "C10S1", "C10S2", "C10S3",
 "C11S1", "C11S2", "C11S3", "C11S4", "C11S4P2",
 "END2", "CRED",
 "VSS1", "VSS2", "VSS3", "VSS4", "VSS5",
-"ZEN1", "ZEN2"
+"ZEN1", "ZEN2"*/
 ]
 
 function randomSong() {
