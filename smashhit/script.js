c1f23c2a0ad56d10eb4e50efb3b12789afde4e46
Glitch (marcusicoi) <none>
2023-08-13 02:10:23 +0000

    🐷🙈 Checkpoint ./smashhit/script.js:27704817/196 ./smashhit/index.html:27704817/1875

--- smashhit/script.js
+++ smashhit/script.js
@@ -9,6 +9,7 @@ let Songs = [
 "C1S1", "C1S2", "C1S3",
 "C2S1", "C2S2", "C2S3", "C2S4",
 "C3S1", "C3S2", "C3S3", "C3S4",
+"C4S1", "C4S2"
 ]
 
 function randomSong() {
@@ -22,10 +23,11 @@ ID("button").style.visibility = "hidden"
 ID(randomSong()).play()
 
 //Next song loop
-for (let i = 1; i < 8172; i++) {
+let i = 1
 setInterval(
-  function() {ID(randomSong()).play()}, 
-  ((35000 * i) - (3000 * i))
-)}
-
+  function() {
+    ID(randomSong()).play()
+    i += 1
+  }, 
+  ((35000 * i) - 3000))
 }
