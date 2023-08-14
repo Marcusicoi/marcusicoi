f5230dd0513522e2bf6b412864cb68ff5db320ff
Glitch (marcusicoi) <none>
2023-08-14 13:21:06 +0000

    🏄💅 Checkpoint ./florr/dps/script.js:27704817/64

--- florr/dps/script.js
+++ florr/dps/script.js
@@ -5,7 +5,11 @@ let rarities = [
   "Common", "Unusual", "Rare", "Epic",
   "Legendary", "Mythic", "Ultra", "Super"
 ]
+
 //Rotation Stats
-let defaultfaster = 2.5
-let  = [2.8, 3.1, 3.4, 3.7, 4, 4.3, 4.6]
+let deffaster = 2.5
+let fasters = [2.8, 3.1, 3.4, 3.7, 4, 4.3, 4.6]
+
+//Reload Stats
+let defreload = 
 
