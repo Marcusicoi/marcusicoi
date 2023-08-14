42c1d032c9e8a326e202214c389fda047fd07d02
Glitch (marcusicoi) <none>
2023-08-14 13:15:51 +0000

    🍕🎯 Checkpoint ./florr/dps/script.js:27704817/317

--- florr/dps/script.js
+++ florr/dps/script.js
@@ -1,3 +1,11 @@
 let faster = document.getElementById("faster")
 let rotation = document.getElementById("rotation")
 let button = document.getElementById("button")
+let rarities = [
+  "Common", "Unusual", "Rare", "Epic",
+  "Legendary", "Mythic", "Ultra", "Super"
+]
+//Rotation Stats
+let defaultfaster = 2.5
+let  = [2.8, 3.1, 3.4, 3.7, 4, 4.3, 4.6]
+
