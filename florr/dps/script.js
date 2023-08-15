5683dcb2464152db06024558f84191a00b74b3f1
Glitch (marcusicoi) <none>
2023-08-15 15:37:39 +0000

    🍜⏳ Checkpoint ./florr/dps/index.html:27704817/116 ./florr/dps/script.js:27704817/308

--- florr/dps/script.js
+++ florr/dps/script.js
@@ -2,6 +2,7 @@ function ID(id) {
   return document.getElementById(id)
 }
 
+//Debug??
 let isDebugMode = false
 
 //Rotation Stats
@@ -14,6 +15,10 @@ let reloads = [0.93, 0.86, 0.8, 0.75, 0.69, 0.65, 0.60, 0.55]
 //DPS Stats 
 let fasterSkill = ID("faster").value
 let reloadSkill = ID("reload").value
-let petalHealth = ID("
+let petalHealth = ID("pethlt").value
+let petalDamage = ID("petdmg").value
+let petalCooldown = ID("petcld").value
+let mobHealth = ID("mobhlt").value
+let mobDamage = ID("mobdmg").value
 
 
