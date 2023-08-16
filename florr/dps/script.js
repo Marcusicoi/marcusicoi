b73d6b4ae01c6db07578197ac6c5060ff39c9111
Glitch (marcusicoi) <none>
2023-08-16 08:11:47 +0000

    🎩🐬 Checkpoint ./florr/dps/script.js:27704817/1641 ./florr/dps/index.html:27704817/264

--- florr/dps/script.js
+++ florr/dps/script.js
@@ -15,32 +15,27 @@ ID("debugbutton").onclick = function() {
 }
 
 //DPS Stats 
-let fasterSkill = 2.5
-let reloadSkill = 1
-let petalHealth = ID("pethlt").value
-let petalDamage = ID("petdmg").value
-let petalCooldown = ID("petcld").value
-let mobHealth = ID("mobhlt").value
-let mobDamage = ID("mobdmg").value
+var rotationSkill, reloadSkill
 
-if (ID("faster").value === "Common") {
-  fasterSkill = 2.8
-} else if (ID("faster").value === "Unusual") {
-  fasterSkill = 3.1
-} else if (ID("faster").value === "Rare") {
-  fasterSkill = 3.4
-} else if (ID("faster").value === "Epic") {
-  fasterSkill = 3.7
-} else if (ID("faster").value === "Legendary") {
-  fasterSkill = 4
-} else if (ID("faster").value === "Mythic") {
-  fasterSkill = 4.3
-} else if (ID("faster").value === "Ultra") {
-  fasterSkill = 4.6
-} else if (ID("faster").value === "Super") {
-  fasterSkill = 4.9
-}
-
+//Rotation
+if (ID("rotation").value === "Common") {
+  rotationSkill = 2.8
+} else if (ID("rotation").value === "Unusual") {
+  rotationSkill = 3.1
+} else if (ID("rotation").value === "Rare") {
+  rotationSkill = 3.4
+} else if (ID("rotation").value === "Epic") {
+  rotationSkill = 3.7
+} else if (ID("rotation").value === "Legendary") {
+  rotationSkill = 4
+} else if (ID("rotation").value === "Mythic") {
+  rotationSkill = 4.3
+} else if (ID("rotation").value === "Ultra") {
+  rotationSkill = 4.6
+} else if (ID("rotation").value === "Super") {
+  rotationSkill = 4.9
+} else { rotationSkill = 2.5 }
+//Reload
 if (ID("reload").value === "Common") {
   reloadSkill = 0.93
 } else if (ID("reload").value === "Unusual") {
@@ -57,7 +52,8 @@ if (ID("reload").value === "Common") {
   reloadSkill = 0.6
 } else if (ID("reload").value === "Super") {
   reloadSkill = 0.54
-}
+} else { reloadSkill = 1 }
+
 //Calculate
 ID("runbutton").onclick = function() {
   //Reload Reduction 
@@ -65,13 +61,13 @@ ID("runbutton").onclick = function() {
   //Mob Stats
   let mobstats = Math.ceil(ID("mobhlt").value / ID("mobdmg").value)
   //Full rotations to reach mob
-  let petrot = Math.ceil((fasterSkill * (petrld + 0.04 * (mobstats - 1))) / (2 * Math.PI))
+  let petrot = Math.ceil((rotationSkill * (petrld + 0.04 * (mobstats - 1))) / (2 * Math.PI))
   //Final
-  let total = (mobstats * ID("petdmg").value) / ((2 * petrot * Math.PI) / fasterSkill)
+  let total = (mobstats * ID("petdmg").value) / ((2 * petrot * Math.PI) / rotationSkill)
   
   //Submit info to the website
-  ID("fastresult").innerHTML = `FASTER SKILL: ${ID("faster").value}`
-  ID("rotateresult").innerHTML = `RELOAD SKILL: ${ID("reload").value}`
+  ID("rotateresult").innerHTML = `ROTATION SKILL: ${ID("rotate").value} ${rotationSkill}`
+  ID("reloadresult").innerHTML = `RELOAD SKILL: ${ID("reload").value}`
   ID("petalused").innerHTML = `PETAL USED: ${ID("petname").value}`
   ID("mobused").innerHTML = `MOB ATTACK: ${ID("mobname").value}`
   ID("totaldps").innerHTML = `TOTAL DPS OF ${ID("petname").value} IS ${total}`
