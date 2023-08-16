a8b2e8c3b5886dad971a51e10a4143f9935cf03e
Glitch (marcusicoi) <none>
2023-08-16 09:52:12 +0000

    🎓💘 Checkpoint ./florr/dps/index.html:27704817/289 ./florr/dps/script.js:27704817/1302

--- florr/dps/script.js
+++ florr/dps/script.js
@@ -14,60 +14,20 @@ ID("debugbutton").onclick = function() {
   }
 }
 
-//DPS Stats 
-var rotationSkill, reloadSkill
-
-//Rotation
-if (ID("rotate").value == "Common") {
-  rotationSkill = 2.8
-} else if (ID("rotate").value == "Unusual") {
-  rotationSkill = 3.1
-} else if (ID("rotate").value == "Rare") {
-  rotationSkill = 3.4
-} else if (ID("rotate").value == "Epic") {
-  rotationSkill = 3.7
-} else if (ID("rotate").value == "Legendary") {
-  rotationSkill = 4
-} else if (ID("rotate").value == "Mythic") {
-  rotationSkill = 4.3
-} else if (ID("rotate").value == "Ultra") {
-  rotationSkill = 4.6
-} else if (ID("rotate").value == "Super") {
-  rotationSkill = 4.9
-} else { rotationSkill = 2.5 }
-//Reload
-if (ID("reload").value == "Common") {
-  reloadSkill = 0.93
-} else if (ID("reload").value == "Unusual") {
-  reloadSkill = 0.86
-} else if (ID("reload").value == "Rare") {
-  reloadSkill = 0.8
-} else if (ID("reload").value == "Epic") {
-  reloadSkill = 0.75
-} else if (ID("reload").value == "Legendary") {
-  reloadSkill = 0.69
-} else if (ID("reload").value == "Mythic") {
-  reloadSkill = 0.65
-} else if (ID("reload").value == "Ultra") {
-  reloadSkill = 0.6
-} else if (ID("reload").value === "Super") {
-  reloadSkill = 0.54
-} else { reloadSkill = 1 }
-
 //Calculate
 ID("runbutton").onclick = function() {
   //Reload Reduction 
-  let petrld = ID("petcld").value * reloadSkill
+  let petrld = ID("petcld").value * ID("reload").value
   //Mob Stats
   let mobstats = Math.ceil(ID("mobhlt").value / ID("mobdmg").value)
   //Full rotations to reach mob
-  let petrot = Math.ceil((rotationSkill * (petrld + 0.04 * (mobstats - 1))) / (2 * Math.PI))
+  let petrot = Math.ceil((ID("rotate").value * (petrld + 0.04 * (mobstats - 1))) / (2 * Math.PI))
   //Final
-  let total = (mobstats * ID("petdmg").value) / ((2 * petrot * Math.PI) / rotationSkill)
+  let total = (mobstats * ID("petdmg").value) / ((2 * petrot * Math.PI) / ID("rotate").value)
   
   //Submit info to the website
-  ID("rotateresult").innerHTML = `ROTATION SKILL: ${ID("rotate").value} ${rotationSkill}`
-  ID("reloadresult").innerHTML = `RELOAD SKILL: ${ID("reload").value}`
+  ID("rotateresult").innerHTML = `ROTATION RADIUS: ${ID("rotate").value}`
+  ID("reloadresult").innerHTML = `RELOAD REDUCTION: ${ID("reload").value}`
   ID("petalused").innerHTML = `PETAL USED: ${ID("petname").value}`
   ID("mobused").innerHTML = `MOB ATTACK: ${ID("mobname").value}`
   ID("totaldps").innerHTML = `TOTAL DPS OF ${ID("petname").value} IS ${total}`
