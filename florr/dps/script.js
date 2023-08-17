fc6c7c29b9108e03fb81905f86b39802a01486fe
Glitch (marcusicoi) <none>
2023-08-17 05:29:30 +0000

    Rewound to commit 08448765c9db0e373e7f5c3b14b5c9a35832b063 + 0 edits

--- florr/dps/script.js
+++ florr/dps/script.js
@@ -89,25 +89,23 @@ ID("runbutton").onclick = function() {
   ID("petalused").innerHTML = `PETAL USED: ${ID("petname").value}`
   ID("mobused").innerHTML = `MOB ATTACK: ${ID("mobname").value}`
   
-  //FIRST STEP
   ID("reloadreduct").innerHTML = `
-  PETAL COOLDOWN:
+  PETAL COOLDOWN: ${petrld} 
   <h6>(${ID("petcld").value} * ${ID("reload").value})</h6>
   <h6>(PetalCooldown * ReloadReduct)</h6>`
   
-  //SECOND STEP
   ID("mobstats").innerHTML = `
-  MOB STATS:
+  MOB STATS: ${mobstats} 
   <h6>ceiling(${ID("mobhlt").value} / ${ID("mobdmg").value})</h6>
   <h6>ceiling(MobHealth / MobDamage)</h6>`
   
   ID("petalrotate").innerHTML = `
-  HITS EACH ROTATION:
+  HITS EACH ROTATION: ${petrot} 
   <h6>ceiling((${ID("rotate").value} * (${petrld} + 0.04 * (${mobstats} - 1))) / (2 * π))</h6>
   <h6>ceiling((RotationSkill * ((PetalCooldown * ReloadReduct) + 0.04 * ((MobHealth / MobDamage) - 1))) / (2 * π))</h6>`
   
   ID("totaldps").innerHTML = `
-  TOTAL DPS OF
+  TOTAL DPS OF ${ID("petnum").value} ${ID("petname").value}(s) FROM ${ID("mobname").value} WITH ${ID("rotate").value} rad/s AND x${ID("reload").value} RELOAD REDUCTION IS ${total} 
   <h6>(${mobstats} * ${ID("petdmg").value} / ((2 * ${petrot} * π) / ${ID("rotate").value}))</h6>
   <h6>((MobHealth / MobDamage) * PetalDamage) / ((2 * HitsEachRotation * π) / RotationSkill</h6>`
 }
