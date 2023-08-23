20026e7b157445da434391b28df0e13097f09c92
Glitch (marcusicoi) <none>
2023-08-23 13:49:18 +0000

    Rewound to commit 0b565b0e6b96b2260aa0cfed0a2b8a101482cf98 + 0 edits

--- smashhit/script.js
+++ smashhit/script.js
@@ -25,96 +25,17 @@ let Songs = [
 function randomSong() {
   return Songs[Math.floor(Math.random() * Songs.length)]
 }
-
-//REVERSED??!
-let isReversed = false
-window.onload = function() { ID("reverseT").style.visibility = "hidden" }
-
-let revSongs = [
-  "Checkpoint0_1.mp3", "Checkpoint0_2.mp3", "Checkpoint0_3.mp3",
-  "Checkpoint1_1.mp3", "Checkpoint1_2.mp3", "Checkpoint2_3.mp3",
-  "Checkpoint2_1.mp3", "Checkpoint2_2.mp3", "Checkpoint2_3.mp3", "Checkpoint2_4.mp3",
-  "Checkpoint3_1.mp3", "Checkpoint3_2.mp3", "Checkpoint3_3.mp3", "Checkpoint3_4.mp3",
-  "Checkpoint4_1.mp3", "Checkpoint4_2.mp3",
-  "Checkpoint5_1.mp3",                      "Checkpoint5_3.mp3", "Checkpoint5_4.mp3",
-  "Checkpoint6_1.mp3", "Checkpoint6_2.mp3", "Checkpoint6_3.mp3", "Checkpoint6_4.mp3",
-  "Checkpoint7_1.mp3", "Checkpoint7_2.mp3", "Checkpoint7_3.mp3",
-                                            "Checkpoint8_3.mp3",
-  "Checkpoint9_1.mp3", "Checkpoint9_2.mp3", "Checkpoint9_3_1.mp3", "Checkpoint9_3_2.mp3",
-  "Checkpoint10_1.mp3", "Checkpoint10_2.mp3", "Checkpoint10_3.mp3",
-  "Checkpoint11_1.mp3", "Checkpoint11_2.mp3", "Checkpoint11_3.mp3", "Checkpoint11_4_1.mp3", "Checkpoint11_4_2.mp3",
-]
-
-function randomReversedSong() {
-  return "/smashhit/songs/" + revSongs[Math.floor(Math.random() * revSongs.length)]                          
-}
-
-function reverseSong(file) {
-// Create an audio context
-const audioContext = new (window.AudioContext || window.webkitAudioContext)();
-
-// Load the audio file
-const audioElement = new Audio(file);
-const audioSource = audioContext.createMediaElementSource(audioElement);
-
-// Create a script processor node
-const scriptNode = audioContext.createScriptProcessor(4096, 1, 1);
-
-// Reverse the audio data
-scriptNode.onaudioprocess = function (audioProcessingEvent) {
-  const inputBuffer = audioProcessingEvent.inputBuffer;
-  const outputBuffer = audioProcessingEvent.outputBuffer;
-
-  for (let channel = 0; channel < outputBuffer.numberOfChannels; channel++) {
-    const inputData = inputBuffer.getChannelData(channel);
-    const outputData = outputBuffer.getChannelData(channel);
-
-    for (let i = 0; i < inputBuffer.length; i++) {
-      outputData[i] = inputData[inputBuffer.length - i - 1];
-    }
-  }
-}
-// Connect the nodes
-audioSource.connect(scriptNode);
-scriptNode.connect(audioContext.destination);
-
-// Start playing the audio
-audioElement.play();
-}
-//Reversed Button
-ID("revbutton").onclick = function() {
-  if (isReversed === false) {
-    isReversed = true
-    ID("reverseT").style.visibility = "visible"
-  } else if (isReversed === true) {
-    isReversed = false
-    ID("reverseT").style.visibility = "hidden"
-  }
-}
 //PLAY!!
-ID("button").onclick = function() {
+ID("button").onclick = function (){
 ID("button").style.visibility = "hidden"
+ID(randomSong()).play()
 
-if (isReversed === false) {
-  ID(randomSong()).play()
-} else if (isReversed === true) {
-  reverseSong(randomReversedSong())
-}
 //Next song loop
 let i = 1
-  if (isReversed === false) {
-  setInterval(
-    function() {
-      ID(randomSong()).play()
-      i += 1
-    }, 
-    ((35000 * i) - 3000))
-  } else if(isReversed === true) {
-    setInterval(
-      function() {
-        reverseSong(randomReversedSong())
-        i += 1
-      },
-      ((35000 * i) - 3000))
-  }
+setInterval(
+  function() {
+    ID(randomSong()).play()
+    i += 1
+  }, 
+  ((35000 * i) - 3000))
 }
