d25f1b0e5bd513d02d3200d80612e44f8c325775
Glitch (marcusicoi) <none>
2023-08-25 08:10:21 +0000

    🌋📒 Checkpoint ./arras/killscore/script.js:27704817/851 ./arras/killscore/index.html:27704817/3

--- arras/killscore/script.js
+++ arras/killscore/script.js
@@ -1,4 +1,9 @@
 //CALCULATE!
 document.getElementById("button").onclick = function() {
-  
+  //Formula
+  function score(num) {
+    return (num > 26300 * 1.5) ? Math.pow(num - 26300, 0.85) + 26300 : num / 1.5                                    
+  }
+  //
+  document.getElementById("result").innerHTML = score(document.getElementById("input").value)
 }
