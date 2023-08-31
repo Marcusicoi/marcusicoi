5d2fbdc3a91ed1a37da32a7a81b9e9c32fd98a5c
Glitch (marcusicoi) <none>
2023-08-31 01:13:47 +0000

    💈🐺 Checkpoint ./florr/buildran/index.html:27704817/87 ./smashhit/script.js:27704817/786

--- smashhit/script.js
+++ smashhit/script.js
@@ -3,6 +3,18 @@ function ID(id) {
   return document.getElementById(id)
 }
 
+//Body Background
+function hex() {
+  let char = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"],
+  x = char[Math.floor(Math.random() * char.length)],
+  i = "#" + x + x + x + x + x + x
+  return i
+}
+let body = {
+  normal: ""
+}
+document.body.style.background = hex()
+
 //Randomize Songs
 let Songs = [
 "C0S1", "C0S2", "C0S3",
