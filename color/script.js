39b74bc3d4d072c229b0b475216d216e6fa1842c
Glitch (marcusicoi) <none>
2023-08-31 06:20:34 +0000

    🐊😻 Checkpoint ./color/script.js:27704817/722 ./color/index.html:27704817/29

--- color/script.js
+++ color/script.js
@@ -3,9 +3,21 @@ function ID(x) {
 }
 
 const changeBG = (x) => {
-  return document.body.style.color = x
+  return document.body.style.background = x
+}
+
+const onclick = (id, x) => {
+  return ID(id).onclick = x
 }
 
 function hex() {
-  let char = ["0", "1", "2", "3", "4", 5"
+  let char = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"]
+  function ran() {
+    return char[Math.floor(Math.random() * char.length)]
+  }
+  let i = "#" + ran() + ran() + ran() + ran() + ran() + ran()
+  ID("cresult").innerHTML = i
+  return i
 }
+
+onclick("button", function() { changeBG(hex())})
