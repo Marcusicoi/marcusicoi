7acbecc28d4a3dca36f71b1b7144835cae9fb62a
Glitch (marcusicoi) <none>
2023-08-31 06:08:33 +0000

    🎯🍯 Checkpoint ./color/index.html:27704817/785 ./color/script.js:27704817/302

---
+++ color/script.js
@@ -0,0 +1,11 @@
+function ID(x) {
+  return document.getElementById(x)
+}
+
+const changeBG = (x) => {
+  return document.body.style.color = x
+}
+
+function hex() {
+  let char = ["0", "1", "2", "3", "4", 5"
+}
