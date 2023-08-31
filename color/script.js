5e9e15297b95e6de9235024b592f9b1e30c1039a
Glitch (marcusicoi) <none>
2023-08-31 06:44:32 +0000

    🎮🙉 Checkpoint ./color/script.js:27704817/2496

--- color/script.js
+++ color/script.js
@@ -16,7 +16,7 @@ function hex() {
     return char[Math.floor(Math.random() * char.length)]
   }
   function rgb(x) {
-    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(x);
     return result ? {
       r: parseInt(result[1], 16),
       g: parseInt(result[2], 16),
@@ -24,26 +24,21 @@ function hex() {
     } : null;
   }
   function cmyk(x) {
-    let computedC = 0,
-        computedM = 0,
-        computedY = 0,
-        computedK = 0
-    hex = (hex.charAt(0)=="#") ? hex.substring(1,7) : hex
-    let r = parseInt(hex.substring(0,2),16),
-        g = parseInt(hex.substring(2,4),16),
-        b = parseInt(hex.substring(4,6),16)
-
- /
- if (r==0 && g==0 && b==0) {
-  computedK = 1;
-  return [0,0,0,1];
- }
-
- computedC = 1 - (r/255);
- computedM = 1 - (g/255);
- computedY = 1 - (b/255);
-
- var minCMY = Math.min(computedC,Math.min(computedM,computedY));
+    let c = 0,
+        m = 0,
+        y = 0,
+        k = 0
+    let r = parseInt(x.substring(0,2),16),
+        g = parseInt(x.substring(2,4),16),
+        b = parseInt(x.substring(4,6),16); 
+    if (r == 0 && g == 0 && b == 0) {
+      k = 1;
+      return `0%, 0%, 0%, 1%`;
+      }
+    c = 1 - (r/255);
+    m = 1 - (g/255);
+    y = 1 - (b/255);
+   let minCMY = Math.min(computedC,Math.min(computedM,computedY));
 
  computedC = (computedC - minCMY) / (1 - minCMY) ;
  computedM = (computedM - minCMY) / (1 - minCMY) ;
@@ -52,8 +47,9 @@ function hex() {
 
  return [computedC,computedM,computedY,computedK];
 }
+  
   let i = "#" + ran() + ran() + ran() + ran() + ran() + ran()
-  ID("cresult").innerHTML = i + ", " + `rgb(${rgb(i).r}, ${rgb(i).g}, ${rgb(i).b})`
+  ID("cresult").innerHTML = `${i}, rgb(${rgb(i).r}, ${rgb(i).g}, ${rgb(i).b}), ${cmyk(i)}` 
   return i
 }
 
